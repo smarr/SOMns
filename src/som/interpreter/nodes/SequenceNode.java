edfac34a445770697a5b4347918f63f713465b74
Stefan Marr <git@stefan-marr.de>
2013-06-26 11:45:59 +0200

    Implemented restart primitive for loops
    - the RestartLoopException is caught and used in normal SequenceNodes
    (was a missing commit)
    [TODO-REPORT: discuss implementation of restart, discuss whether this is actually the correct place to implement it]
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/SequenceNode.java
+++ src/som/interpreter/nodes/SequenceNode.java
@@ -15,10 +15,24 @@ public class SequenceNode extends ExpressionNode {
   public Object executeGeneric(VirtualFrame frame) {
     Object last = null;
     
-    for (ExpressionNode expression : expressions) {
-      last = expression.executeGeneric(frame);
-    }
+    boolean restart;
     
+    do {
+      restart = false;
+
+      try {
+        for (ExpressionNode expression : expressions) {
+          last = expression.executeGeneric(frame);
+        }
+      }
+      catch (RestartLoopException e) {
+        // TODO: figure out whether this is the best way to implement the
+        // restart primitive, because it is in a pretty generic place for a
+        // rather specific thing.
+        restart = true;
+      }
+    } while (restart);
+      
     return last;
   }
 
