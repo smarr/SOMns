78be091f582d2b48ea8bf73defad4c057dad07a0
Stefan Marr <git@stefan-marr.de>
2013-06-26 09:58:04 +0200

    Added initial set of simple interpreter tests
    - these tests allow me to debug more targeted the different mechanisms
    - the standard test suite already requires the base system to work much more stable than what I got at the moment, because it relies on System.som being functional to load classes
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ tests/som/tests/BasicInterpreterTests.java
@@ -0,0 +1,72 @@
+package som.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import som.vm.Universe;
+import som.vmobjects.Integer;
+
+@RunWith(Parameterized.class)
+public class BasicInterpreterTests {
+
+  @Parameters
+  public static Iterable<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        { "MethodCall",     "test",  42, Integer.class },
+        { "MethodCall",     "test2", 42, Integer.class },
+        { "NonLocalReturn", "test",  "NonLocalReturn", som.vmobjects.Class.class },
+        { "NonLocalReturn", "test1", 42, Integer.class },
+        { "NonLocalReturn", "test2", 43, Integer.class },
+        { "NonLocalReturn", "test3",  3, Integer.class },
+    }); 
+  }
+  
+  private final String testClass;
+  private final String testSelector;
+  private final Object expectedResult;
+  private final Class<?> resultType;
+  
+  public BasicInterpreterTests(final String testClass,
+      final String testSelector,
+      final Object expectedResult,
+      final Class<?> resultType) {
+    this.testClass      = testClass;
+    this.testSelector   = testSelector;
+    this.expectedResult = expectedResult;
+    this.resultType     = resultType;
+  }
+  
+  protected void assertEqualsSOMValue(Object expectedResult, Object actualResult) {
+    if (resultType == Integer.class) {
+      int expected = ((java.lang.Integer)expectedResult).intValue();
+      int actual   = ((Integer)actualResult).getEmbeddedInteger();
+      assertEquals(expected, actual);
+      return;
+    }
+    
+    if (resultType == som.vmobjects.Class.class) {
+      String expected = (String)expectedResult;
+      String actual   = ((som.vmobjects.Class)actualResult).getName().getString();
+      assertEquals(expected, actual);
+      return;
+    }
+    fail("SOM Value handler missing");
+  }
+  
+  @Test
+  public void testBasicInterpreterBehavior() {
+    
+    Universe u = new Universe(true);
+    u.setupClassPath("Smalltalk:BasicInterpreterTests");
+    
+    Object actualResult = u.interpret(testClass, testSelector);
+    
+    assertEqualsSOMValue(expectedResult, actualResult);
+  }
+}
