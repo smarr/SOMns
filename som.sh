f4ff4d8cf9716a53774142733b2692ce7422d176
Stefan Marr <git@stefan-marr.de>
2013-10-01 14:29:04 +0200

    Added Truffle-DSL jar to classpath
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++ som.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # -G:TruffleCompilationThreshold=3 -Xbootclasspath/a:build/classes \
-java -server -cp build/classes:libs/com.oracle.truffle.api.jar \
+java -server -cp build/classes:libs/com.oracle.truffle.api.jar:libs/com.oracle.truffle.api.dsl.jar \
         som.vm.Universe \
 		"$@"
