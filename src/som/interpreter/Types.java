357b3f47e4457d437ff5730575e1112ee1856b9d
Stefan Marr <git@stefan-marr.de>
2013-10-01 14:42:14 +0200

    Added the first working type system.
    For some reason, previous attempts always failed.
    
    My current understanding is that we need the type system in order to map SOM types to Java natives, avoiding boxing. Especially to utilize the return-type specialization approach of Truffle.
    
    Problematic is I think at the moment that the generated type system uses automatically generated asObject() and isObject() functions, and does override the once I provide.
    
    In the literal nodes for Integer, String, and BigInteger, I needed to manually add an executeGeneric, which is more optimal than the generated one, but seems to point out problems with the overriding of asObject()/isObject().
    
    [TODO-REPORT: investigate how the use of the type system works out eventually, report the problems with the overriding]
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/Types.java
+++ src/som/interpreter/Types.java
@@ -21,9 +21,155 @@
  */
 package som.interpreter;
 
+import java.math.BigInteger;
+
+import som.vm.Universe;
+
+import com.oracle.truffle.api.dsl.TypeCast;
+import com.oracle.truffle.api.dsl.TypeCheck;
 import com.oracle.truffle.api.dsl.TypeSystem;
 
-@TypeSystem({som.vmobjects.Object.class})
+@TypeSystem({       int.class,
+             BigInteger.class,
+                boolean.class,
+                 String.class,
+                 double.class,
+   som.vmobjects.Object.class})
 public class Types {
 
+  @TypeCheck
+  public boolean isInteger(Object value) {
+    return value instanceof Integer               ||
+           value instanceof som.vmobjects.Integer ||
+          (value instanceof BigInteger && ((BigInteger) value).bitLength() < Integer.SIZE) ||
+          (value instanceof som.vmobjects.BigInteger &&
+              ((som.vmobjects.BigInteger) value).getEmbeddedBiginteger().bitLength() < Integer.SIZE);
+  }
+
+  @TypeCast
+  public int asInteger(Object value) {
+    assert isInteger(value);
+    if (value instanceof Integer) {
+      return (int) value;
+    } else if (value instanceof som.vmobjects.Integer) {
+      return ((som.vmobjects.Integer) value).getEmbeddedInteger();
+    } else {
+      BigInteger val;
+      if (value instanceof BigInteger) {
+        val = (BigInteger) value;
+      } else {
+        val = ((som.vmobjects.BigInteger) value).getEmbeddedBiginteger();
+      }
+      int result = val.intValue();
+      assert BigInteger.valueOf(result).equals(value) : "Losing precision";
+      return result;
+    }
+  }
+
+  @TypeCheck
+  public boolean isBigInteger(Object value) {
+    return value instanceof BigInteger ||
+           value instanceof som.vmobjects.BigInteger;
+  }
+
+  @TypeCast
+  public BigInteger asBigInteger(Object value) {
+    assert isBigInteger(value);
+    if (value instanceof BigInteger) {
+      return (BigInteger) value;
+    } else {
+      return ((som.vmobjects.BigInteger) value).getEmbeddedBiginteger();
+    }
+  }
+
+  @TypeCheck
+  public boolean isBoolean(Object value) {
+    return value instanceof Boolean ||
+           Universe.current().trueObject == value ||
+           Universe.current().falseObject == value;
+  }
+
+  @TypeCast
+  public boolean asBoolean(Object value) {
+    assert isBoolean(value);
+    if (value instanceof Boolean) {
+      return (boolean) value;
+    } else if (Universe.current().trueObject == value) {
+      return true;
+    } else {
+      assert Universe.current().falseObject == value;
+      return false;
+    }
+  }
+
+  @TypeCheck
+  public boolean isString(Object value) {
+    return value instanceof String ||
+           value instanceof som.vmobjects.String;
+  }
+
+  @TypeCast
+  public String asString(Object value) {
+    assert isString(value);
+    if (value instanceof String) {
+      return (String) value;
+    } else {
+      return ((som.vmobjects.String) value).getEmbeddedString();
+    }
+  }
+
+  @TypeCheck
+  public boolean isDouble(Object value) {
+    return value instanceof Double ||
+           value instanceof som.vmobjects.Double;
+  }
+
+  @TypeCast
+  public double asDouble(Object value) {
+    assert isDouble(value);
+    if (value instanceof Double) {
+      return (double) value;
+    } else {
+      return ((som.vmobjects.Double) value).getEmbeddedDouble();
+    }
+  }
+
+  // TODO: clear with truffle team why this is overridden in the generated class
+  @TypeCheck
+  public boolean isObject(java.lang.Object value) {
+    return value instanceof som.vmobjects.Object; // ||
+//           value instanceof Boolean ||
+//           value instanceof Double  ||
+//           value instanceof Integer ||
+//           value instanceof BigInteger ||
+//           value instanceof String;
+  }
+
+  // TODO: why is that currently not really supported (isObject is overridden)
+  // TODO: this is also problematic, because I need to do the 'conversion' explicitly in all executeGeneric()
+  @TypeCast
+  public som.vmobjects.Object asObject(Object value) {
+    assert isObject(value);
+    if (value instanceof som.vmobjects.Object) {
+      return (som.vmobjects.Object) value;
+    } else {
+      Universe u = Universe.current();
+      if (value instanceof Boolean) {
+        if ((boolean) value) {
+          return u.trueObject;
+        } else {
+          return u.falseObject;
+        }
+      } else if (value instanceof Double) {
+        return u.newDouble((double) value);
+      } else if (value instanceof Integer) {
+        return u.newInteger((int) value);
+      } else if (value instanceof BigInteger) {
+        return u.newBigInteger((BigInteger) value);
+      } else {
+        assert value instanceof String;
+        return u.newString((String) value);
+      }
+    }
+  }
 }
