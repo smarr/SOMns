afb2776551a5c8549ea7dc4bd0b904c39dd249d6
Stefan Marr <git@stefan-marr.de>
2013-10-30 16:39:10 -0400

    Added missing Primitives to access Primitive fields
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/som/primitives/PrimitivePrimitives.java
@@ -0,0 +1,18 @@
+package som.primitives;
+
+import som.primitives.MethodPrimsFactory.HolderPrimFactory;
+import som.primitives.MethodPrimsFactory.SignaturePrimFactory;
+import som.vm.Universe;
+
+
+public class PrimitivePrimitives extends Primitives {
+  public PrimitivePrimitives(final Universe universe) {
+    super(universe);
+  }
+
+  @Override
+  public void installPrimitives() {
+    installInstancePrimitive("signature", SignaturePrimFactory.getInstance());
+    installInstancePrimitive("holder", HolderPrimFactory.getInstance());
+  }
+}
