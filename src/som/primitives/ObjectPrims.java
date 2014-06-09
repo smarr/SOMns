6b9a19482857ba2ee233871d2b360f07a31c3460
Stefan Marr <git@stefan-marr.de>
2014-06-09 16:18:04 +0200

    Move #perform primitives to separate package
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/ObjectPrims.java
+++ src/som/primitives/ObjectPrims.java
@@ -1,7 +1,6 @@
 package som.primitives;
 
 import som.interpreter.Types;
-import som.interpreter.nodes.nary.BinaryExpressionNode;
 import som.interpreter.nodes.nary.BinaryExpressionNode.BinarySideEffectFreeExpressionNode;
 import som.interpreter.nodes.nary.TernaryExpressionNode;
 import som.interpreter.nodes.nary.UnaryExpressionNode;
@@ -9,54 +8,12 @@ import som.interpreter.nodes.nary.UnaryExpressionNode.UnarySideEffectFreeExpress
 import som.vm.Universe;
 import som.vmobjects.SAbstractObject;
 import som.vmobjects.SClass;
-import som.vmobjects.SInvokable;
 import som.vmobjects.SObject;
-import som.vmobjects.SSymbol;
 
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.frame.VirtualFrame;
 
 
 public final class ObjectPrims {
-  public abstract static class PerformPrim extends BinaryExpressionNode {
-    private final Universe universe;
-    public PerformPrim() { super(null); this.universe = Universe.current(); }
-
-    @Specialization
-    public final Object doObject(final VirtualFrame frame, final Object receiver, final SSymbol selector) {
-      SInvokable invokable = Types.getClassOf(receiver, universe).lookupInvokable(selector);
-      return invokable.invoke(receiver);
-    }
-  }
-
-  public abstract static class PerformInSuperclassPrim extends TernaryExpressionNode {
-    @Specialization
-    public final Object doSAbstractObject(final VirtualFrame frame,
-        final SAbstractObject receiver, final SSymbol selector, final SClass  clazz) {
-      SInvokable invokable = clazz.lookupInvokable(selector);
-      return invokable.invoke(receiver);
-    }
-  }
-
-  public abstract static class PerformWithArgumentsPrim extends TernaryExpressionNode {
-    private final Universe universe;
-    public PerformWithArgumentsPrim() { this.universe = Universe.current(); }
-
-    @Specialization
-    public final Object doObject(final VirtualFrame frame,
-        final Object receiver, final SSymbol selector, final Object[]  argsArr) {
-      SInvokable invokable = Types.getClassOf(receiver, universe).lookupInvokable(selector);
-
-      // need to unwrap argsArr and create a new Object array including the
-      // receiver
-      Object[] args = new Object[argsArr.length + 1];
-      args[0] = receiver;
-      System.arraycopy(argsArr, 0, args, 1, argsArr.length);
-
-      return invokable.invoke(args);
-    }
-  }
-
   public abstract static class InstVarAtPrim extends BinarySideEffectFreeExpressionNode {
     @Specialization
     public final Object doSObject(final SObject receiver, final long idx) {
---
+++ src/som/primitives/reflection/PerformInSuperclassPrim.java
@@ -0,0 +1,19 @@
+package som.primitives.reflection;
+
+import som.interpreter.nodes.nary.TernaryExpressionNode;
+import som.vmobjects.SAbstractObject;
+import som.vmobjects.SClass;
+import som.vmobjects.SInvokable;
+import som.vmobjects.SSymbol;
+
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+
+public abstract class PerformInSuperclassPrim extends TernaryExpressionNode {
+  @Specialization
+  public final Object doSAbstractObject(final VirtualFrame frame,
+      final SAbstractObject receiver, final SSymbol selector, final SClass  clazz) {
+    SInvokable invokable = clazz.lookupInvokable(selector);
+    return invokable.invoke(receiver);
+  }
+}
