80e11b290ae1300fc9011dd7741daaee8a8d86cb
Stefan Marr <git@stefan-marr.de>
2014-06-09 20:56:57 +0200

    Added specialization for #doesNotUnderstand
    This is not yet as fast as it should be. The benchmark shows roughly double the runtime of what it should be.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/SArguments.java
+++ src/som/interpreter/SArguments.java
@@ -2,6 +2,7 @@ package som.interpreter;
 
 import com.oracle.truffle.api.frame.Frame;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.ExplodeLoop;
 
 public final class SArguments {
 
@@ -19,6 +20,7 @@ public final class SArguments {
    * Create a properly encoded SArguments array to be passed via Truffle's API
    * from a receiver object that is separate from the actual arguments.
    */
+  @ExplodeLoop
   public static Object[] createSArgumentsArrayFrom(final Object receiver, final Object[] argsArray) {
     // below, we have a lot of magic numbers and implicit positioning,
     // which are all based on this assumption
@@ -31,7 +33,29 @@ public final class SArguments {
     Object[] arguments = new Object[argsArray.length + 1];
     arguments[RCVR_IDX] = receiver;
 
-    System.arraycopy(argsArray, 0, arguments, 1, argsArray.length);
+//    System.arraycopy(argsArray, 0, arguments, 1, argsArray.length);
+
+    for (int i = 0; i < argsArray.length; i++) {
+      arguments[i + 1] = argsArray[i];
+    }
+
     return arguments;
+  }
+
+  /**
+   * Create a new array from an SArguments array that contains only the true
+   * arguments and excludes the receiver. This is used for instance for
+   * #doesNotUnderstand (#dnu)
+   */
+  @ExplodeLoop
+  public static Object[] getArgumentsWithoutReceiver(final Object[] arguments) {
+    // the code and magic numbers below are based on the following assumption
+    assert RCVR_IDX == 0;
+//    return Arrays.copyOfRange(arguments, 1, arguments.length);
+    Object[] argsArr = new Object[arguments.length - 1];
+    for (int i = 1; i < arguments.length; i++) {
+      argsArr[i - 1] = arguments[i];
+    }
+    return argsArr;
   }
 }
