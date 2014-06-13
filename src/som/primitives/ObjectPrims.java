588fc2f6f333ca879e853f0deae29edfa9e5c4e6
Stefan Marr <git@stefan-marr.de>
2014-06-13 18:31:38 +0200

    Fixed breakpoint primitive to work on all objects [BACKPORT!]
    This was broken since I removed SInteger etc.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/ObjectPrims.java
+++ src/som/primitives/ObjectPrims.java
@@ -49,7 +49,7 @@ public final class ObjectPrims {
   public abstract static class HaltPrim extends UnaryExpressionNode {
     public HaltPrim() { super(null, false); } /* TODO: enforced!!! */
     @Specialization
-    public final SAbstractObject doSAbstractObject(final SAbstractObject receiver) {
+    public final Object doSAbstractObject(final Object receiver) {
       Universe.errorPrintln("BREAKPOINT");
       return receiver;
     }
