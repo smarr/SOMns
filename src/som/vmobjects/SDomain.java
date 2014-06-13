f862b1acc7360466e032971b732f91961bac37b6
Stefan Marr <git@stefan-marr.de>
2014-06-13 14:27:08 +0200

    Implemented domain primitives
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/SDomain.java
+++ src/som/vmobjects/SDomain.java
@@ -3,18 +3,21 @@ package som.vmobjects;
 
 public final class SDomain {
 
+  public static final int NEW_OBJECT_DOMAIN_IDX = 0;
+  private static final int NUM_SDOMAIN_FIELDS = NEW_OBJECT_DOMAIN_IDX + 1;
+
   public static SObject createStandardDomain(final SObject nilObject) {
-    SObject domain = SObject.create(nilObject, nilObject, 1);
+    SObject domain = SObject.create(nilObject, nilObject, NUM_SDOMAIN_FIELDS);
     domain.setDomain(domain);
     setDomainForNewObjects(domain, domain);
     return domain;
   }
 
   public static SObject getDomainForNewObjects(final SObject domain) {
-    return domain..
+    return domain.getSDomainDomainForNewObjects();
   }
 
   public static void setDomainForNewObjects(final SObject domain, final SObject newObjectDomain) {
-    domain.. = newObjectDomain;
+    domain.setSDomainDomainForNewObjects(newObjectDomain);
   }
 }
