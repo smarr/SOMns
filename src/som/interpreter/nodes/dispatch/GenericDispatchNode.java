0cfe317569972551cb3397dcd887913f30ecb9ac
Stefan Marr <git@stefan-marr.de>
2014-06-16 15:48:13 +0200

    Move lookup to only user: GenericDispatchNode, and mark two help as @SlowPath to avoid compilation.
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/dispatch/GenericDispatchNode.java
+++ src/som/interpreter/nodes/dispatch/GenericDispatchNode.java
@@ -1,10 +1,13 @@
 package som.interpreter.nodes.dispatch;
 
+import som.interpreter.Types;
 import som.vm.Universe;
 import som.vmobjects.SAbstractObject;
+import som.vmobjects.SClass;
 import som.vmobjects.SInvokable;
 import som.vmobjects.SSymbol;
 
+import com.oracle.truffle.api.CompilerDirectives.SlowPath;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 public final class GenericDispatchNode extends AbstractDispatchWithLookupNode {
@@ -26,6 +29,13 @@ public final class GenericDispatchNode extends AbstractDispatchWithLookupNode {
     }
   }
 
+  @SlowPath
+  private SInvokable lookupMethod(final Object rcvr) {
+    SClass rcvrClass = Types.getClassOf(rcvr, universe);
+    return rcvrClass.lookupInvokable(selector);
+  }
+
+  @SlowPath
   private Object sendDoesNotUnderstand(final Object[] arguments) {
     // TODO: this is all extremely expensive, and could be optimized by
     //       further specialization for #dnu
