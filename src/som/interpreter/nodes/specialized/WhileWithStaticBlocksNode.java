7828c9ea06e1fa1eeca27e6285cf5d2d283167da
Stefan Marr <git@stefan-marr.de>
2014-06-16 16:23:49 +0200

    Refactor out common parts for while loops
    Signed-off-by: Stefan Marr <git@stefan-marr.de>
    
    Conflicts:
    	src/som/interpreter/nodes/specialized/WhileWithDynamicBlocksNode.java
    	src/som/interpreter/nodes/specialized/WhileWithStaticBlocksNode.java

--- src/som/interpreter/nodes/specialized/WhileWithStaticBlocksNode.java
+++ src/som/interpreter/nodes/specialized/WhileWithStaticBlocksNode.java
@@ -1,55 +1,25 @@
 package som.interpreter.nodes.specialized;
 
-import som.interpreter.Invokable;
-import som.interpreter.SArguments;
 import som.interpreter.nodes.literals.BlockNode;
-import som.interpreter.nodes.nary.BinaryExpressionNode;
 import som.vm.Universe;
 import som.vmobjects.SBlock;
 import som.vmobjects.SObject;
 
-import com.oracle.truffle.api.CallTarget;
-import com.oracle.truffle.api.CompilerAsserts;
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.SourceSection;
-import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.VirtualFrame;
-import com.oracle.truffle.api.nodes.DirectCallNode;
-import com.oracle.truffle.api.nodes.Node;
-import com.oracle.truffle.api.nodes.RootNode;
 
 
-public abstract class WhileWithStaticBlocksNode extends BinaryExpressionNode {
+public abstract class WhileWithStaticBlocksNode extends AbstractWhileNode {
   @Child protected BlockNode receiver;
   @Child protected BlockNode argument;
-  @Child protected DirectCallNode  conditionValueSend;
-  @Child protected DirectCallNode  bodyValueSend;
-  private final boolean conditionEnforced;
-  private final boolean bodyEnforced;
-
-  protected final boolean predicateBool;
-  private   final Universe universe;
 
   private WhileWithStaticBlocksNode(final BlockNode receiver,
       final BlockNode argument, final SBlock rcvr, final SBlock arg,
       final boolean predicateBool, final Universe universe,
       final SourceSection source, final boolean executesEnforced) {
-    super(source, executesEnforced);
+    super(rcvr, arg, predicateBool, universe, source, executesEnforced);
     this.receiver = receiver;
     this.argument = argument;
-
-    CallTarget callTargetCondition = rcvr.getMethod().getCallTarget();
-    conditionValueSend = Truffle.getRuntime().createDirectCallNode(
-        callTargetCondition);
-    conditionEnforced = rcvr.isEnforced();
-
-    CallTarget callTargetBody = arg.getMethod().getCallTarget();
-    bodyValueSend = Truffle.getRuntime().createDirectCallNode(
-        callTargetBody);
-    bodyEnforced = arg.isEnforced();
-
-    this.predicateBool = predicateBool;
-    this.universe      = universe;
   }
 
   @Override
@@ -67,57 +37,10 @@ public abstract class WhileWithStaticBlocksNode extends BinaryExpressionNode {
   }
 
   @Override
-  public final Object executeEvaluated(final VirtualFrame frame,
-      final Object rcvr, final Object arg) {
-    return doWhileConditionally(frame, (SBlock) rcvr, (SBlock) arg);
-  }
-
-  @Override
-  public final void executeEvaluatedVoid(final VirtualFrame frame,
-      final Object rcvr, final Object arg) {
-    doWhileConditionally(frame, (SBlock) rcvr, (SBlock) arg);
-  }
-
   protected final SObject doWhileConditionally(final VirtualFrame frame,
       final SBlock loopCondition,
       final SBlock loopBody) {
-    long iterationCount = 0;
-    SObject domain = SArguments.domain(frame);
-    boolean loopConditionResult = (boolean) conditionValueSend.call(
-        frame, SArguments.createSArguments(domain, conditionEnforced, new Object[] {loopCondition}));
-
-
-    try {
-      // TODO: this is a simplification, we don't cover the case receiver isn't a boolean
-      while (loopConditionResult == predicateBool) {
-        bodyValueSend.call(frame, SArguments.createSArguments(domain,
-            bodyEnforced, new Object[] {loopBody}));
-        loopConditionResult = (boolean) conditionValueSend.call(
-            frame, SArguments.createSArguments(domain, conditionEnforced,
-                new Object[] {loopCondition}));
-
-        if (CompilerDirectives.inInterpreter()) {
-          iterationCount++;
-        }
-      }
-    } finally {
-      if (CompilerDirectives.inInterpreter()) {
-        reportLoopCount(iterationCount);
-      }
-    }
-
-    return universe.nilObject;
-  }
-
-  protected final void reportLoopCount(final long count) {
-    CompilerAsserts.neverPartOfCompilation();
-    Node current = getParent();
-    while (current != null && !(current instanceof RootNode)) {
-      current = current.getParent();
-    }
-    if (current != null) {
-      ((Invokable) current).propagateLoopCountThroughoutLexicalScope(count);
-    }
+    return doWhileUnconditionally(frame, loopCondition, loopBody);
   }
 
   public static final class WhileTrueStaticBlocksNode extends WhileWithStaticBlocksNode {
