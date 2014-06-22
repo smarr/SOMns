7f26eece8df5689fdd41c6d78705d7289891246e
Stefan Marr <git@stefan-marr.de>
2014-06-22 18:33:47 +0200

    Added minimal test for Set
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/som/tests/SomTests.java
+++ tests/som/tests/SomTests.java
@@ -58,6 +58,7 @@ public class SomTests {
         {"SelfBlock"     },
         {"Super"         },
 
+        {"Set"           },
         {"String"        },
         {"Symbol"        },
         {"System"        },
