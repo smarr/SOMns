8f00ebac9f37f48a1283244e134e9f50aaa4322f
Stefan Marr <git@stefan-marr.de>
2014-07-02 18:17:38 +0200

    Changed WhilePrimitiveNode to use the WhileCache
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/som/interpreter/nodes/specialized/whileloops/WhileCache.java
@@ -0,0 +1,147 @@
+package som.interpreter.nodes.specialized.whileloops;
+
+import static som.interpreter.TruffleCompiler.transferToInterpreterAndInvalidate;
+import som.interpreter.SArguments;
+import som.vm.Universe;
+import som.vmobjects.SBlock;
+import som.vmobjects.SInvokable;
+import som.vmobjects.SObject;
+
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.Node;
+
+
+public final class WhileCache {
+  public static final int INLINE_CACHE_SIZE = 6;
+
+  public static AbstractWhileDispatch create(final boolean predicateBool, final boolean executesEnforced) {
+    return new UninitializedDispatch(predicateBool, executesEnforced, 0);
+  }
+
+  public abstract static class AbstractWhileDispatch extends Node {
+    protected final boolean predicateBool;
+    protected final boolean executesEnforced;
+    protected final int depth;
+
+
+    public AbstractWhileDispatch(final boolean predicateBool,
+        final boolean executesEnforced, final int depth) {
+      this.predicateBool    = predicateBool;
+      this.executesEnforced = executesEnforced;
+      this.depth            = depth;
+    }
+
+    public abstract SObject executeDispatch(VirtualFrame frame,
+        SBlock loopCondition, SBlock loopBody);
+  }
+
+  public static final class UninitializedDispatch extends AbstractWhileDispatch {
+
+    public UninitializedDispatch(final boolean predicateBool,
+        final boolean executesEnforced, final int depth) {
+      super(predicateBool, executesEnforced, depth);
+    }
+
+    @Override
+    public SObject executeDispatch(final VirtualFrame frame,
+        final SBlock loopCondition, final SBlock loopBody) {
+      transferToInterpreterAndInvalidate("Initialize a dispatch node.");
+
+      boolean enforced = SArguments.enforced(frame);
+
+      if (depth < INLINE_CACHE_SIZE) {
+        CachedDispatch specialized = new CachedDispatch(loopCondition, loopBody,
+            predicateBool, enforced, depth);
+        return replace(specialized).
+            executeDispatch(frame, loopCondition, loopBody);
+      }
+
+      AbstractWhileDispatch headNode = determineChainHead();
+      GenericDispatch generic = new GenericDispatch(predicateBool, executesEnforced);
+      return headNode.replace(generic).
+          executeDispatch(frame, loopCondition, loopBody);
+    }
+
+    private AbstractWhileDispatch determineChainHead() {
+      Node i = this;
+      while (i.getParent() instanceof AbstractWhileDispatch) {
+        i = i.getParent();
+      }
+      return (AbstractWhileDispatch) i;
+    }
+  }
+
+  public static final class CachedDispatch extends AbstractWhileDispatch {
+
+    private final SInvokable condition;
+    private final SInvokable body;
+    @Child private WhileWithDynamicBlocksNode whileNode;
+    @Child private AbstractWhileDispatch next;
+
+    public CachedDispatch(final SBlock condition, final SBlock body,
+        final boolean predicateBool, final boolean executesEnforced,
+        final int depth) {
+      super(predicateBool, executesEnforced, depth);
+      this.condition = condition.getMethod();
+      this.body      = body.getMethod();
+      this.next = new UninitializedDispatch(predicateBool, executesEnforced, depth + 1);
+      this.whileNode = new WhileWithDynamicBlocksNode(condition, body,
+          predicateBool, Universe.current(), null, executesEnforced);
+    }
+
+    @Override
+    public SObject executeDispatch(final VirtualFrame frame, final SBlock loopCondition,
+        final SBlock loopBody) {
+      if (condition == loopCondition.getMethod() &&
+          body      == loopBody.getMethod()) {
+        return whileNode.doWhileUnconditionally(frame, loopCondition, loopBody);
+      } else {
+        return next.executeDispatch(frame, loopCondition, loopBody);
+      }
+    }
+  }
+
+  public static final class GenericDispatch extends AbstractWhileDispatch {
+    private final Universe universe;
+
+    public GenericDispatch(final boolean predicateBool, final boolean executesEnforced) {
+      super(predicateBool, executesEnforced, 0);
+      this.universe = Universe.current();
+    }
+
+    private boolean obj2bool(final Object o) {
+      if (o instanceof Boolean) {
+        return (boolean) o;
+      } else if (o == universe.trueObject) {
+        CompilerAsserts.neverPartOfCompilation();
+        return true;
+      } else {
+        CompilerAsserts.neverPartOfCompilation();
+        assert o == universe.falseObject;
+        return false;
+      }
+    }
+
+    @Override
+    public SObject executeDispatch(final VirtualFrame frame, final SBlock loopCondition,
+        final SBlock loopBody) {
+      CompilerAsserts.neverPartOfCompilation(); // no caching, direct invokes, no loop count reporting...
+
+      SObject currentDomain = SArguments.domain(frame);
+
+      Object conditionResult = loopCondition.getMethod().invoke(currentDomain, loopCondition.isEnforced(), loopCondition);
+      boolean loopConditionResult = obj2bool(conditionResult);
+
+
+      // TODO: this is a simplification, we don't cover the case receiver isn't a boolean
+      while (loopConditionResult == predicateBool) {
+        loopBody.getMethod().invoke(currentDomain, loopBody.isEnforced(), loopBody);
+        conditionResult = loopCondition.getMethod().invoke(currentDomain, loopCondition.isEnforced(), loopCondition);
+        loopConditionResult = obj2bool(conditionResult);
+      }
+      return universe.nilObject;
+    }
+
+  }
+}
--- src/som/interpreter/nodes/specialized/whileloops/WhilePrimitiveNode.java
+++ src/som/interpreter/nodes/specialized/whileloops/WhilePrimitiveNode.java
@@ -1,61 +1,32 @@
 package som.interpreter.nodes.specialized.whileloops;
 
-import som.interpreter.SArguments;
 import som.interpreter.nodes.nary.BinaryExpressionNode;
-import som.vm.Universe;
+import som.interpreter.nodes.specialized.whileloops.WhileCache.AbstractWhileDispatch;
 import som.vmobjects.SBlock;
 import som.vmobjects.SObject;
 
-import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 
 
 public abstract class WhilePrimitiveNode extends BinaryExpressionNode {
   final boolean predicateBool;
-  final Universe universe;
+  @Child protected AbstractWhileDispatch whileNode;
 
   protected WhilePrimitiveNode(final boolean executesEnforced, final boolean predicateBool) {
     super(null, executesEnforced);
-    universe = Universe.current();
     this.predicateBool = predicateBool;
+    this.whileNode = WhileCache.create(predicateBool, executesEnforced);
   }
 
   protected WhilePrimitiveNode(final WhilePrimitiveNode node) {
     this(node.executesEnforced, node.predicateBool);
   }
 
-  private boolean obj2bool(final Object o) {
-    if (o instanceof Boolean) {
-      return (boolean) o;
-    } else if (o == universe.trueObject) {
-      CompilerAsserts.neverPartOfCompilation();
-      return true;
-    } else {
-      CompilerAsserts.neverPartOfCompilation();
-      assert o == universe.falseObject;
-      return false;
-    }
-  }
-
   @Specialization
   protected SObject doWhileConditionally(final VirtualFrame frame,
       final SBlock loopCondition, final SBlock loopBody) {
-    CompilerAsserts.neverPartOfCompilation(); // no caching, direct invokes, no loop count reporting...
-
-    SObject currentDomain = SArguments.domain(frame);
-
-    Object conditionResult = loopCondition.getMethod().invoke(currentDomain, loopCondition.isEnforced(), loopCondition);
-    boolean loopConditionResult = obj2bool(conditionResult);
-
-
-    // TODO: this is a simplification, we don't cover the case receiver isn't a boolean
-    while (loopConditionResult == predicateBool) {
-      loopBody.getMethod().invoke(currentDomain, loopBody.isEnforced(), loopBody);
-      conditionResult = loopCondition.getMethod().invoke(currentDomain, loopCondition.isEnforced(), loopCondition);
-      loopConditionResult = obj2bool(conditionResult);
-    }
-    return universe.nilObject;
+    return whileNode.executeDispatch(frame, loopCondition, loopBody);
   }
 
   @Override
