6424b3ac72d6dda901603c90fe1ac999abdba7e8
Stefan Marr <git@stefan-marr.de>
2014-08-18 15:36:57 +0200

    Added specialization for #downTo:do:
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/MessageSendNode.java
+++ src/som/interpreter/nodes/MessageSendNode.java
@@ -15,6 +15,7 @@ import som.interpreter.nodes.specialized.AndMessageNodeFactory.AndBoolMessageNod
 import som.interpreter.nodes.specialized.IfFalseMessageNodeFactory;
 import som.interpreter.nodes.specialized.IfTrueIfFalseMessageNodeFactory;
 import som.interpreter.nodes.specialized.IfTrueMessageNodeFactory;
+import som.interpreter.nodes.specialized.IntDownToDoMessageNodeFactory;
 import som.interpreter.nodes.specialized.IntToByDoMessageNodeFactory;
 import som.interpreter.nodes.specialized.IntToDoMessageNodeFactory;
 import som.interpreter.nodes.specialized.NotMessageNodeFactory;
@@ -342,6 +343,16 @@ public final class MessageSendNode {
                   TypesGen.TYPES.isDouble(arguments[1])) &&
               TypesGen.TYPES.isSBlock(arguments[2])) {
             return replace(IntToDoMessageNodeFactory.create(this,
+                (SBlock) arguments[2], argumentNodes[0], argumentNodes[1],
+                argumentNodes[2]));
+          }
+          break;
+        case "downTo:do:":
+          if (TypesGen.TYPES.isLong(arguments[0]) &&
+              (TypesGen.TYPES.isLong(arguments[1]) ||
+                  TypesGen.TYPES.isDouble(arguments[1])) &&
+              TypesGen.TYPES.isSBlock(arguments[2])) {
+            return replace(IntDownToDoMessageNodeFactory.create(this,
                 (SBlock) arguments[2], argumentNodes[0], argumentNodes[1],
                 argumentNodes[2]));
           }
---
+++ src/som/interpreter/nodes/specialized/IntDownToDoMessageNode.java
@@ -0,0 +1,100 @@
+package som.interpreter.nodes.specialized;
+
+import som.interpreter.Invokable;
+import som.interpreter.nodes.ExpressionNode;
+import som.interpreter.nodes.PreevaluatedExpression;
+import som.interpreter.nodes.nary.TernaryExpressionNode;
+import som.vmobjects.SBlock;
+import som.vmobjects.SInvokable;
+
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.SlowPath;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.DirectCallNode;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RootNode;
+
+
+public abstract class IntDownToDoMessageNode extends TernaryExpressionNode
+    implements PreevaluatedExpression {
+
+  private final SInvokable blockMethod;
+  @Child private DirectCallNode valueSend;
+
+  public IntDownToDoMessageNode(final ExpressionNode orignialNode,
+      final SBlock block) {
+    super(orignialNode.getSourceSection());
+    blockMethod = block.getMethod();
+    valueSend = Truffle.getRuntime().createDirectCallNode(
+                    blockMethod.getCallTarget());
+  }
+
+  public IntDownToDoMessageNode(final IntDownToDoMessageNode node) {
+    super(node.getSourceSection());
+    this.blockMethod = node.blockMethod;
+    this.valueSend   = node.valueSend;
+  }
+
+  @Override
+  public final Object doPreEvaluated(final VirtualFrame frame,
+      final Object[] arguments) {
+    return executeEvaluated(frame, arguments[0], arguments[1], arguments[2]);
+  }
+
+  protected final boolean isSameBlockLong(final long receiver, final long limit, final SBlock block) {
+    return block.getMethod() == blockMethod;
+  }
+
+  @Specialization(guards = "isSameBlockLong")
+  public final long doIntDownToDo(final VirtualFrame frame, final long receiver, final long limit, final SBlock block) {
+    try {
+      if (receiver >= limit) {
+        valueSend.call(frame, new Object[] {block, receiver});
+      }
+      for (long i = receiver - 1; i >= limit; i--) {
+        valueSend.call(frame, new Object[] {block, i});
+      }
+    } finally {
+      if (CompilerDirectives.inInterpreter() && (limit - receiver) > 0) {
+        reportLoopCount(limit - receiver);
+      }
+    }
+    return receiver;
+  }
+
+  protected final boolean isSameBlockDouble(final long receiver, final double limit, final SBlock block) {
+    return block.getMethod() == blockMethod;
+  }
+
+  @Specialization(guards = "isSameBlockDouble")
+  public final long doIntDownToDo(final VirtualFrame frame, final long receiver, final double limit, final SBlock block) {
+    try {
+      if (receiver >= limit) {
+        valueSend.call(frame, new Object[] {block, receiver});
+      }
+      for (long i = receiver - 1; i >= limit; i--) {
+        valueSend.call(frame, new Object[] {block, i});
+      }
+    } finally {
+      if (CompilerDirectives.inInterpreter()) {
+        reportLoopCount((int) limit - receiver);
+      }
+    }
+    return receiver;
+  }
+
+  @SlowPath
+  private void reportLoopCount(final long count) {
+    CompilerAsserts.neverPartOfCompilation("reportLoopCount");
+    Node current = getParent();
+    while (current != null && !(current instanceof RootNode)) {
+      current = current.getParent();
+    }
+    if (current != null) {
+      ((Invokable) current).propagateLoopCountThroughoutLexicalScope(count);
+    }
+  }
+}
