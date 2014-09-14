ff9fe1a1a607cb8601ce31a2443d9e4ce40d81a8
Stefan Marr <git@stefan-marr.de>
2014-09-14 21:55:27 +0200

    Added IndexDispatch and optimize #instVarAt:*
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/ObjectPrims.java
+++ src/som/primitives/ObjectPrims.java
@@ -5,40 +5,79 @@ import som.interpreter.nodes.nary.BinaryExpressionNode.BinarySideEffectFreeExpre
 import som.interpreter.nodes.nary.TernaryExpressionNode;
 import som.interpreter.nodes.nary.UnaryExpressionNode;
 import som.interpreter.nodes.nary.UnaryExpressionNode.UnarySideEffectFreeExpressionNode;
+import som.primitives.reflection.IndexDispatch;
 import som.vm.Universe;
 import som.vmobjects.SAbstractObject;
 import som.vmobjects.SClass;
 import som.vmobjects.SObject;
 import som.vmobjects.SSymbol;
 
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
 
 
 public final class ObjectPrims {
   public abstract static class InstVarAtPrim extends BinarySideEffectFreeExpressionNode {
+
+    @Child private IndexDispatch dispatch;
+
+    public InstVarAtPrim() {
+      super();
+      dispatch = IndexDispatch.create();
+    }
+    public InstVarAtPrim(final InstVarAtPrim node) { this(); }
+
     @Specialization
     public final Object doSObject(final SObject receiver, final long idx) {
-      return receiver.getField(idx - 1);
+      return dispatch.executeDispatch(receiver, (int) idx - 1);
+    }
+
+    @Override
+    public final Object executeEvaluated(final VirtualFrame frame,
+      final Object receiver, final Object firstArg) {
+      assert receiver instanceof SObject;
+      assert firstArg instanceof Long;
+
+      SObject rcvr = CompilerDirectives.unsafeCast(receiver, SObject.class, true, true);
+      long idx     = CompilerDirectives.unsafeCast(firstArg, long.class, true, true);
+      return doSObject(rcvr, idx);
     }
   }
 
   public abstract static class InstVarAtPutPrim extends TernaryExpressionNode {
-    @Specialization
-    public final Object doSObject(final SObject receiver, final long idx, final SAbstractObject val) {
-      receiver.setField(idx - 1, val);
-      return val;
+    @Child private IndexDispatch dispatch;
+
+    public InstVarAtPutPrim() {
+      super();
+      dispatch = IndexDispatch.create();
     }
+    public InstVarAtPutPrim(final InstVarAtPutPrim node) { this(); }
 
     @Specialization
     public final Object doSObject(final SObject receiver, final long idx, final Object val) {
-      receiver.setField(idx - 1, val);
+      dispatch.executeDispatch(receiver, (int) idx - 1, val);
       return val;
+    }
+
+    @Override
+    public final Object executeEvaluated(final VirtualFrame frame,
+      final Object receiver, final Object firstArg, final Object secondArg) {
+      assert receiver instanceof SObject;
+      assert firstArg instanceof Long;
+      assert secondArg != null;
+
+      SObject rcvr = CompilerDirectives.unsafeCast(receiver, SObject.class, true, true);
+      long idx     = CompilerDirectives.unsafeCast(firstArg, long.class, true, true);
+      return doSObject(rcvr, idx, secondArg);
     }
   }
 
   public abstract static class InstVarNamedPrim extends BinarySideEffectFreeExpressionNode {
     @Specialization
     public final Object doSObject(final SObject receiver, final SSymbol fieldName) {
+      CompilerAsserts.neverPartOfCompilation();
       return receiver.getField(receiver.getFieldIndex(fieldName));
     }
   }
