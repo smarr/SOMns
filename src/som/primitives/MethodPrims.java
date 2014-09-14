9fd901cf27f655946434a8d1168b3131cee6f923
Stefan Marr <git@stefan-marr.de>
2014-09-14 21:59:07 +0200

    Optimized #invokeOn:
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/MethodPrims.java
+++ src/som/primitives/MethodPrims.java
@@ -1,10 +1,14 @@
 package som.primitives;
 
+import som.interpreter.SArguments;
+import som.interpreter.nodes.dispatch.InvokeOnCache;
+import som.interpreter.nodes.nary.TernaryExpressionNode;
 import som.interpreter.nodes.nary.UnaryExpressionNode.UnarySideEffectFreeExpressionNode;
 import som.vmobjects.SAbstractObject;
 import som.vmobjects.SInvokable;
 
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
 
 
 public final class MethodPrims {
@@ -20,6 +24,22 @@ public final class MethodPrims {
     @Specialization
     public final SAbstractObject doSMethod(final SInvokable receiver) {
       return receiver.getHolder();
+    }
+  }
+
+  public abstract static class InvokeOnPrim extends TernaryExpressionNode {
+    @Child private InvokeOnCache callNode;
+
+    public InvokeOnPrim() {
+      callNode = InvokeOnCache.create();
+    }
+    public InvokeOnPrim(final InvokeOnPrim node) { this(); }
+
+    @Specialization
+    public final Object doInvoke(final VirtualFrame frame,
+        final SInvokable receiver, final Object target, final Object[] argsArr) {
+      return callNode.executeDispatch(frame, receiver,
+          SArguments.createSArgumentsArrayFrom(target, argsArr));
     }
   }
 }
--- src/som/primitives/PrimitivePrimitives.java
+++ src/som/primitives/PrimitivePrimitives.java
@@ -1,13 +1,15 @@
 package som.primitives;
 
 import som.primitives.MethodPrimsFactory.HolderPrimFactory;
+import som.primitives.MethodPrimsFactory.InvokeOnPrimFactory;
 import som.primitives.MethodPrimsFactory.SignaturePrimFactory;
 
 
 public final class PrimitivePrimitives extends Primitives {
   @Override
   public void installPrimitives() {
-    installInstancePrimitive("signature", SignaturePrimFactory.getInstance());
-    installInstancePrimitive("holder", HolderPrimFactory.getInstance());
+    installInstancePrimitive("signature",      SignaturePrimFactory.getInstance());
+    installInstancePrimitive("holder",         HolderPrimFactory.getInstance());
+    installInstancePrimitive("invokeOn:with:", InvokeOnPrimFactory.getInstance());
   }
 }
