f71a147fc5a316615f38625a07107c656d3a9439
Stefan Marr <git@stefan-marr.de>
2015-05-14 12:11:58 +0200

    Use Bootstrap entry point
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++ som.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # -G:TruffleCompilationThreshold=3 -Xbootclasspath/a:build/classes \
 java -server -cp build/classes:libs/truffle.jar \
-        som.vm.Universe \
+        som.vm.Bootstrap --platform core-lib/Platform.som \
 		"$@"
