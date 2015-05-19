6e39f8ce38e191bd2b71f30a6099a613c5164d75
Stefan Marr <git@stefan-marr.de>
2015-05-19 18:24:22 +0200

    Added coment about order requirement to AccessModifier
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/compiler/AccessModifier.java
+++ src/som/compiler/AccessModifier.java
@@ -2,5 +2,9 @@ package som.compiler;
 
 
 public enum AccessModifier {
-  PUBLIC, PROTECTED, PRIVATE, NOT_APPLICABLE
+  NOT_APPLICABLE,
+
+  // NOTE: Order is important, for the lookup in SClass, we rely on it to
+  //       define the sets of protected+public vs. public methods
+  PRIVATE, PROTECTED, PUBLIC
 }
