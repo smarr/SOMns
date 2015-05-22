bd5a72981211622657b5333d112282420bf30006
Stefan Marr <git@stefan-marr.de>
2015-05-22 13:06:51 +0200

    Added @Primitive annotation for initialization of vmMirror
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/som/primitives/Primitive.java
@@ -0,0 +1,17 @@
+package som.primitives;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Primitive {
+
+  /** Name of the selector, for which the primitive is to be installed. */
+  String value();
+  // TODO: additional hints for instantiation
+
+}
