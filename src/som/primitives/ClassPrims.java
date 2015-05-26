af8112a385c8c3b880bcd4f843f0c5804f96b228
Stefan Marr <git@stefan-marr.de>
2015-05-26 23:40:35 +0200

    Added #mirrorClassName: primitive
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/ClassPrims.java
+++ src/som/primitives/ClassPrims.java
@@ -1,9 +1,11 @@
 package som.primitives;
 
+import som.interpreter.Types;
 import som.interpreter.nodes.nary.UnaryExpressionNode;
 import som.vmobjects.SAbstractObject;
 import som.vmobjects.SClass;
 
+import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 
@@ -17,6 +19,16 @@ public class ClassPrims {
     @Specialization
     public final SAbstractObject doSClass(final SClass receiver) {
       return receiver.getName();
+    }
+  }
+
+  @GenerateNodeFactory
+  @Primitive("mirrorClassName:")
+  public abstract static class ClassNamePrim extends UnaryExpressionNode {
+    @Specialization
+    public final SAbstractObject doSClass(final Object receiver) {
+      CompilerAsserts.neverPartOfCompilation("should specialize, to avoid Types.getClassOf()");
+      return Types.getClassOf(receiver).getName();
     }
   }
 
