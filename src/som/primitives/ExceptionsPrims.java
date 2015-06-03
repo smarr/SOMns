dbe78bdff17cb3ca77ac6dc40d7d13e8166d0343
Stefan Marr <git@stefan-marr.de>
2015-06-03 10:55:01 +0200

    Rewrite ExceptionDoOnPrim using DSL
    Previously, it was not having a generic case in case the limit on the PIC was reached. The ValuePrimitiveNode doesn't allow for multiple heads...
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/ExceptionsPrims.java
+++ src/som/primitives/ExceptionsPrims.java
@@ -10,10 +10,15 @@ import som.primitives.BlockPrims.ValuePrimitiveNode;
 import som.vmobjects.SAbstractObject;
 import som.vmobjects.SBlock;
 import som.vmobjects.SClass;
+import som.vmobjects.SInvokable;
 
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.DirectCallNode;
+import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.nodes.NodeCost;
 
 
@@ -21,19 +26,34 @@ public abstract class ExceptionsPrims {
 
   @GenerateNodeFactory
   @Primitive("exceptionDo:catch:onException:")
-  public abstract static class ExceptionDoOnPrim extends TernaryExpressionNode implements ValuePrimitiveNode {
+  public abstract static class ExceptionDoOnPrim extends TernaryExpressionNode {
 
-    @Child private AbstractDispatchNode dispatchBody    = new UninitializedValuePrimDispatchNode();
-    @Child private AbstractDispatchNode dispatchHandler = new UninitializedValuePrimDispatchNode();
+    protected static final int INLINE_CACHE_SIZE = 6;
+    protected static final IndirectCallNode indirect = Truffle.getRuntime().createIndirectCallNode();
 
-    @Specialization
+    public static final DirectCallNode createCallNode(final SBlock block) {
+      return Truffle.getRuntime().createDirectCallNode(
+          block.getMethod().getCallTarget());
+    }
+
+    public static final boolean sameBlock(final SBlock block, final SInvokable method) {
+      return block.getMethod() == method;
+    }
+
+    @Specialization(limit = "INLINE_CACHE_SIZE",
+        guards = {"sameBlock(body, cachedBody)",
+                  "sameBlock(exceptionHandler, cachedExceptionMethod)"})
     public final Object doException(final VirtualFrame frame, final SBlock body,
-        final SClass exceptionClass, final SBlock exceptionHandler) {
+        final SClass exceptionClass, final SBlock exceptionHandler,
+        @Cached("body.getMethod()") final SInvokable cachedBody,
+        @Cached("createCallNode(body)") final DirectCallNode bodyCall,
+        @Cached("exceptionHandler.getMethod()") final SInvokable cachedExceptionMethod,
+        @Cached("createCallNode(exceptionHandler)") final DirectCallNode exceptionCall) {
       try {
-        return dispatchBody.executeDispatch(frame, new Object[] {body});
+        return bodyCall.call(frame, new Object[] {body});
       } catch (SomException e) {
         if (e.getSomObject().getSOMClass().isKindOf(exceptionClass)) {
-          return dispatchHandler.executeDispatch(frame,
+          return exceptionCall.call(frame,
               new Object[] {exceptionHandler, e.getSomObject()});
         } else {
           throw e;
@@ -41,24 +61,18 @@ public abstract class ExceptionsPrims {
       }
     }
 
-    @Override
-    public final void adoptNewDispatchListHead(final AbstractDispatchNode node) {
-      throw new RuntimeException("This is not supported. Need to use a "
-          + "different way, perhaps the DSL instead. but the "
-          + "ValuePrimitiveNode interface doesn't work for two blocks.");
-    }
-
-    @Override
-    public NodeCost getCost() {
-      int dispatchChain = dispatchBody.lengthOfDispatchChain();
-      if (dispatchChain == 0) {
-        return NodeCost.UNINITIALIZED;
-      } else if (dispatchChain == 1) {
-        return NodeCost.MONOMORPHIC;
-      } else if (dispatchChain <= AbstractDispatchNode.INLINE_CACHE_SIZE) {
-        return NodeCost.POLYMORPHIC;
-      } else {
-        return NodeCost.MEGAMORPHIC;
+    @Specialization(contains = "doException")
+    public final Object doExceptionUncached(final VirtualFrame frame, final SBlock body,
+        final SClass exceptionClass, final SBlock exceptionHandler) {
+      try {
+        return body.getMethod().invoke(frame, indirect, new Object[] {body});
+      } catch (SomException e) {
+        if (e.getSomObject().getSOMClass().isKindOf(exceptionClass)) {
+          return exceptionHandler.getMethod().invoke(frame, indirect,
+              new Object[] {exceptionHandler, e.getSomObject()});
+        } else {
+          throw e;
+        }
       }
     }
   }
