d656853534bdf6040929e9763a67713b358fbfe5
Stefan Marr <git@stefan-marr.de>
2015-06-18 15:04:56 -0700

    Optimizing the Array AllocProfile a little
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/arrays/NewPrim.java
+++ src/som/primitives/arrays/NewPrim.java
@@ -10,6 +10,8 @@ import som.vmobjects.SArray;
 import som.vmobjects.SClass;
 
 import com.oracle.truffle.api.Assumption;
+import com.oracle.truffle.api.CompilerAsserts;
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
@@ -26,7 +28,7 @@ public abstract class NewPrim extends BinaryExpressionNode {
   public NewPrim(final NewPrim clone) { allocProfile = clone.allocProfile; }
 
   public static class AllocProfile {
-    @CompilationFinal public Assumption assumption;
+    public final Assumption assumption;
     @CompilationFinal private boolean becomesObject;
 
     public AllocProfile() {
@@ -36,34 +38,38 @@ public abstract class NewPrim extends BinaryExpressionNode {
     public boolean isBecomingObject() { return becomesObject; }
     public void doesBecomeObject() {
       if (!becomesObject) {
-        assumption.invalidate();
-        /* assumption = Truffle.getRuntime().createAssumption(); */
-        becomesObject = true;
+        CompilerDirectives.transferToInterpreter();
+        allocationShouldAlwaysCreateObjectArrays();
       }
     }
-  }
 
-  public final boolean isBecomingObject(final SClass receiver) {
-    return allocProfile.isBecomingObject();
-  }
-
-  public final boolean isNotBecomingObject(final SClass receiver) {
-    return !allocProfile.isBecomingObject();
+    protected void allocationShouldAlwaysCreateObjectArrays() {
+      CompilerAsserts.neverPartOfCompilation("Slow path: allocationShouldAlwaysCreateObjectArrays");
+      assumption.invalidate();
+      /* assumption = Truffle.getRuntime().createAssumption(); */
+      becomesObject = true;
+    }
   }
 
   protected static final boolean receiverIsArrayClass(final SClass receiver) {
     return receiver == Classes.arrayClass;
   }
 
+  protected final boolean assumptionValid(final SClass rcvr) {
+    return allocProfile.assumption.isValid();
+  }
+
+  // TODO: use:
   @Specialization(assumptions = "allocProfile.assumption",
-      guards = {"isNotBecomingObject(receiver)", "receiverIsArrayClass(receiver)"})
+      guards = {"assumptionValid(receiver)",
+      "!allocProfile.isBecomingObject()", "receiverIsArrayClass(receiver)"})
   public final SArray doSpecializingArray(final SClass receiver, final long length) {
     return new SArray(length, allocProfile);
   }
 
   @Specialization(/* does not need the assumption, because it is not changing anymore.
                      assumptions = "allocProfile.assumption", */
-      guards = {"isBecomingObject(receiver)", "receiverIsArrayClass(receiver)"})
+      guards = {"allocProfile.isBecomingObject()", "receiverIsArrayClass(receiver)"})
   public final SArray doObjectArray(final SClass receiver, final long length) {
     Object[] storage = new Object[(int) length];
     Arrays.fill(storage, Nil.nilObject);
