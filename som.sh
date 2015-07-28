f58085335a705978d087f5735aca6f886f08e81c
Stefan Marr <git@stefan-marr.de>
2015-07-28 14:13:43 +0200

    Better to enable assertions in the interpreter script
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++ som.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-java -server -cp build/classes:libs/truffle/build/truffle-api.jar \
+java -server -ea -esa -cp build/classes:libs/truffle/build/truffle-api.jar \
         som.VM --platform core-lib/Platform.som \
         "$@"
