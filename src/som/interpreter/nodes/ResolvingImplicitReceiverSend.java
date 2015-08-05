63ad01a815dab6c454e9e676b2bf4c898da6ee16
Stefan Marr <git@stefan-marr.de>
2015-08-06 01:13:29 +0200

    Handle double execution of rewriting by keeping a reference to the node's replacement
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/ResolvingImplicitReceiverSend.java
+++ src/som/interpreter/nodes/ResolvingImplicitReceiverSend.java
@@ -16,6 +16,9 @@ public class ResolvingImplicitReceiverSend extends AbstractMessageSendNode {
   private final MethodScope currentScope;
   private final ClassDefinitionId classDefId;
 
+  // this is only a helper field, used to handle the specialization race
+  private PreevaluatedExpression replacedBy;
+
   public ResolvingImplicitReceiverSend(final SSymbol selector,
       final ExpressionNode[] arguments, final MethodScope currentScope,
       final ClassDefinitionId classDefId, final SourceSection source) {
@@ -27,6 +30,12 @@ public class ResolvingImplicitReceiverSend extends AbstractMessageSendNode {
 
   @Override
   public Object doPreEvaluated(final VirtualFrame frame, final Object[] args) {
+    // the specialize() is racy and needs to be handled with care.
+    // it rewrites existing nodes and adapts the args array
+    // in some cases
+    //
+    // specifically, it takes the argumentNode[0] node and move it one level
+    // down, under another Outer node
     PreevaluatedExpression newNode = atomic(() -> specialize(args));
     return newNode.
         doPreEvaluated(frame, args);
@@ -38,19 +47,32 @@ public class ResolvingImplicitReceiverSend extends AbstractMessageSendNode {
     // it it is, we get the context level of the outer send and rewrite to one
     ClassIdAndContextLevel result = currentScope.lookupSlotOrClass(selector);
     if (result != null) {
-      assert result.contextLevel >= 0;
-      OuterObjectRead outer = OuterObjectReadNodeGen.create(result.contextLevel,
-          classDefId, result.classId, getSourceSection(), argumentNodes[0]);
-      argumentNodes[0] = outer;
-      newNode = MessageSendNode.createMessageSend(selector, argumentNodes,
-          getSourceSection());
-      replace((ExpressionNode) newNode);
-      args[0] = outer.executeEvaluated(args[0]);
+      if (replacedBy == null) {
+        assert result.contextLevel >= 0;
+        OuterObjectRead outer = OuterObjectReadNodeGen.create(result.contextLevel,
+            classDefId, result.classId, getSourceSection(), argumentNodes[0]);
+        argumentNodes[0] = insert(outer);
+        newNode = MessageSendNode.createMessageSend(selector, argumentNodes,
+            getSourceSection());
+        replace((ExpressionNode) newNode);
+        args[0] = outer.executeEvaluated(args[0]);
+        replacedBy = newNode;
+      } else {
+        // this can happen as part of a race condition
+        // and it means argumentNodes[0] has already been replaced
+        // and we can use it to determine the outer receiver, but don't
+        // need to do much else
+        args[0] = ((OuterObjectRead) argumentNodes[0]).executeEvaluated(args[0]);
+        newNode = replacedBy;
+      }
     } else {
-      // no outer send, so, must be a self send
-      newNode = MessageSendNode.createMessageSend(selector, argumentNodes,
-          getSourceSection());
-      replace((ExpressionNode) newNode);
+      if (replacedBy == null) {
+        newNode = MessageSendNode.createMessageSend(selector, argumentNodes,
+            getSourceSection());
+        replace((ExpressionNode) newNode);
+      } else {
+        newNode = replacedBy;
+      }
     }
     return newNode;
   }
