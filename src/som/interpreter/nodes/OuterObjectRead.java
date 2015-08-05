79ef82774ec228efcfd431c29b963c9e7cc231c5
Stefan Marr <git@stefan-marr.de>
2015-08-05 23:22:13 +0200

    Revert "Try to make OuterObjectRead more robust by relying on structural information"
    This reverts commit 70b2c4fb6f2c1b045c1748d179c4d9d8786e4371.
    
    We can't use the structural/lexical information, because we actually cache the concrete class object to be able to access its enclosing scope object.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/OuterObjectRead.java
+++ src/som/interpreter/nodes/OuterObjectRead.java
@@ -1,7 +1,6 @@
 package som.interpreter.nodes;
 
 import som.compiler.ClassBuilder.ClassDefinitionId;
-import som.compiler.ClassDefinition;
 import som.vm.constants.KernelObj;
 import som.vmobjects.SClass;
 import som.vmobjects.SObjectWithoutFields;
@@ -49,9 +48,7 @@ public abstract class OuterObjectRead
   public boolean isSuperSend() { return false; }
 
   protected final SClass getLexicalClass(final SObjectWithoutFields rcvr) {
-    SClass lexicalClass = rcvr.getSOMClass().getClassCorrespondingTo(classDefId);
-    assert lexicalClass != null;
-    return lexicalClass;
+    return rcvr.getSOMClass().getClassCorrespondingTo(classDefId);
   }
 
   @Specialization(guards = "contextLevel == 0")
@@ -60,9 +57,9 @@ public abstract class OuterObjectRead
   }
 
   @Specialization(limit = "INLINE_CACHE_SIZE",
-      guards = {"contextLevel != 0", "receiver.getSOMClass().getClassDefinition() == rcvrClassDef"})
+      guards = {"contextLevel != 0", "receiver.getSOMClass() == rcvrClass"})
   public final Object doForFurtherOuter(final SObjectWithoutFields receiver,
-      @Cached("receiver.getSOMClass().getClassDefinition()") final ClassDefinition rcvrClassDef,
+      @Cached("receiver.getSOMClass()") final SClass rcvrClass,
       @Cached("getLexicalClass(receiver)") final SClass lexicalClass) {
     return getEnclosingObject(receiver, lexicalClass);
   }
@@ -86,7 +83,6 @@ public abstract class OuterObjectRead
   @ExplodeLoop
   private Object getEnclosingObject(final SObjectWithoutFields receiver,
       final SClass lexicalClass) {
-    assert lexicalClass != null;
     int ctxLevel = contextLevel - 1; // 0 is already covered with specialization
     SObjectWithoutFields enclosing = lexicalClass.getEnclosingObject();
 
