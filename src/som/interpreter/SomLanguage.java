a69a54c8e6fb8e54a1815603d8341e359d43644d
Stefan Marr <git@stefan-marr.de>
2016-01-26 11:53:22 +0100

    Basic implementation of SomLanguage to use PolyglotEngine for startup
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/SomLanguage.java
+++ src/som/interpreter/SomLanguage.java
@@ -2,39 +2,118 @@ package som.interpreter;
 
 import java.io.IOException;
 
+import som.VM;
+import som.compiler.Lexer.SourceCoordinate;
+import som.compiler.MixinBuilder;
+import som.compiler.MixinBuilder.MixinDefinitionError;
+import som.compiler.MixinDefinition;
+import som.compiler.Parser;
+import som.compiler.Parser.ParseError;
 import som.vm.NotYetImplementedException;
+import som.vm.constants.Nil;
+import som.vmobjects.SClass;
 
 import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.frame.MaterializedFrame;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.impl.FindContextNode;
 import com.oracle.truffle.api.instrument.Visualizer;
 import com.oracle.truffle.api.instrument.WrapperNode;
 import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.source.Source;
 
 
-/** TODO: implement. We don't really have a context. So, I set C==Object here */
-public class SomLanguage extends TruffleLanguage<Object> {
+@TruffleLanguage.Registration(name = "SOMns", version = "0.1.0",
+                              mimeType = "application/x-newspeak-som-ns")
+public final class SomLanguage extends TruffleLanguage<VM> {
 
-  @Override
-  protected Object createContext(final Env env) {
-    throw new NotYetImplementedException();
+  public static final String MIME_TYPE = "application/x-newspeak-som-ns";
+  public static final String CMD_ARGS  = "command-line-arguments";
+
+  public static final SomLanguage INSTANCE = new SomLanguage();
+
+  private static final class ParseResult extends RootNode {
+
+    private final SClass moduleClass;
+
+    public ParseResult(final SClass moduleClass) {
+      super(SomLanguage.class, null, null);
+      this.moduleClass = moduleClass;
+    }
+
+    @Override
+    public Object execute(final VirtualFrame frame) {
+      return moduleClass;
+    }
   }
 
   @Override
-  protected CallTarget parse(final Source code, final Node context, final String... argumentNames)
-      throws IOException {
-    throw new NotYetImplementedException();
+  protected VM createContext(final Env env) {
+    VM vm;
+    try {
+      vm = new VM((String[]) env.getConfig().get(MIME_TYPE).get(CMD_ARGS), true);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed accessing kernel or platform code of SOMns.", e);
+    }
+    vm.initalize();
+    return vm;
+  }
+
+  public static final Source START = Source.fromNamedText(
+      "", "Don't Parse, just start execution with command line arguments").withMimeType(MIME_TYPE);
+
+  private static class StartInterpretation extends RootNode {
+
+    private final FindContextNode<VM> contextNode;
+
+    protected StartInterpretation() {
+      super(SomLanguage.class, null, null);
+      contextNode = new FindContextNode<VM>(SomLanguage.class);
+    }
+
+    @Override
+    public Object execute(final VirtualFrame frame) {
+      VM vm = contextNode.executeFindContext();
+      vm.execute();
+      return Nil.nilObject;
+    }
+  }
+
+  private CallTarget createStartCallTarget() {
+    return Truffle.getRuntime().createCallTarget(new StartInterpretation());
   }
 
   @Override
-  protected Object findExportedSymbol(final Object context, final String globalName,
+  protected CallTarget parse(final Source code, final Node context,
+      final String... argumentNames) throws IOException {
+    if (code == START) {
+      return createStartCallTarget();
+    }
+
+    Parser parser = new Parser(code.getReader(), code.getLength(), code);
+    SourceCoordinate coord = parser.getCoordinate();
+
+    try {
+      MixinBuilder moduleBuilder = parser.moduleDeclaration();
+      MixinDefinition moduleDef = moduleBuilder.assemble(parser.getSource(coord));
+      ParseResult result = new ParseResult(moduleDef.instantiateModuleClass());
+      return Truffle.getRuntime().createCallTarget(result);
+    } catch (ParseError | MixinDefinitionError e) {
+      throw new IOException(e);
+    }
+  }
+
+  @Override
+  protected Object findExportedSymbol(final VM context, final String globalName,
       final boolean onlyExplicit) {
     throw new NotYetImplementedException();
   }
 
   @Override
-  protected Object getLanguageGlobal(final Object context) {
+  protected Object getLanguageGlobal(final VM context) {
     throw new NotYetImplementedException();
   }
 
