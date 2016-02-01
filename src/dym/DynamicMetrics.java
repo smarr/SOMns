550989b694f7732bbb6384e00b29746b782090cd
Stefan Marr <git@stefan-marr.de>
2016-02-01 14:13:08 +0100

    Added wrapping for new tags
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/dym/DynamicMetrics.java
+++ src/dym/DynamicMetrics.java
@@ -3,6 +3,8 @@ package dym;
 import java.util.HashMap;
 import java.util.Map;
 
+import som.compiler.Tags;
+
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.instrumentation.EventContext;
 import com.oracle.truffle.api.instrumentation.EventNode;
@@ -13,8 +15,10 @@ import com.oracle.truffle.api.instrumentation.TruffleInstrument;
 import com.oracle.truffle.api.instrumentation.TruffleInstrument.Registration;
 import com.oracle.truffle.api.source.SourceSection;
 
+import dym.nodes.ControlFlowProfileNode;
 import dym.nodes.CountingNode;
 import dym.nodes.InvocationProfilingNode;
+import dym.profiles.BranchProfile;
 import dym.profiles.Counter;
 import dym.profiles.InvocationProfile;
 import dym.profiles.MethodCallsiteProbe;
@@ -32,11 +36,22 @@ import dym.profiles.MethodCallsiteProbe;
 public class DynamicMetrics extends TruffleInstrument {
 
   public static final String ID       = "dym-dynamic-metrics";
-  public static final String ROOT_TAG = "ROOT";
+
+  // Tags used by the DynamicMetrics tool
+  public static final String ROOT_TAG           = "ROOT";
   public static final String UNSPECIFIED_INVOKE = "UNSPECIFIED_INVOKE"; // this is some form of invoke in the source, unclear what it is during program execution
   public static final String INVOKE_WITH_LOOKUP = "INVOKE_WITH_LOOKUP";
   public static final String NEW_OBJECT         = "NEW_OBJECT";
   public static final String NEW_ARRAY          = "NEW_ARRAY";
+  public static final String CONTROL_FLOW_CONDITION  = "CONTROL_FLOW_CONDITION"; // a condition expression that results in a control-flow change
+
+  // TODO
+  public static final String FIELD_READ         = "FIELD_READ";
+  public static final String FIELD_WRITE        = "FIELD_WRITE";
+  public static final String ARRAY_READ         = "ARRAY_READ";
+  public static final String ARRAY_WRITE        = "ARRAY_WRITE";
+  public static final String LOOP_BODY          = "LOOP_BODY";
+
 
   private final Map<SourceSection, InvocationProfile> methodInvocationCounter;
   private int methodStackDepth;
@@ -44,11 +59,15 @@ public class DynamicMetrics extends TruffleInstrument {
 
   private final Map<SourceSection, MethodCallsiteProbe> methodCallsiteProbes;
   private final Map<SourceSection, Counter> instantiationCounter;
+  private final Map<SourceSection, Counter> fieldAccessCounter;
+  private final Map<SourceSection, BranchProfile> controlFlowProfiles;
 
   public DynamicMetrics() {
     methodInvocationCounter = new HashMap<>();
     methodCallsiteProbes    = new HashMap<>();
     instantiationCounter    = new HashMap<>();
+    fieldAccessCounter      = new HashMap<>();
+    controlFlowProfiles     = new HashMap<>();
 
     assert "DefaultTruffleRuntime".equals(
         Truffle.getRuntime().getClass().getSimpleName())
@@ -92,6 +111,26 @@ public class DynamicMetrics extends TruffleInstrument {
           Counter counter = instantiationCounter.computeIfAbsent(
               context.getInstrumentedSourceSection(), src -> new Counter(src));
           return new CountingNode(counter);
+        });
+
+    filters = SourceSectionFilter.newBuilder();
+    filters.tagIs(FIELD_READ); // TODO: FIELD_WRITE
+    instrumenter.attachFactory(
+        filters.build(),
+        (final EventContext context) -> {
+          Counter counter = fieldAccessCounter.computeIfAbsent(
+              context.getInstrumentedSourceSection(), src -> new Counter(src));
+          return new CountingNode(counter);
+        });
+
+    filters = SourceSectionFilter.newBuilder();
+    filters.tagIs(Tags.CONTROL_FLOW_CONDITION);
+    instrumenter.attachFactory(
+        filters.build(),
+        (final EventContext context) -> {
+          BranchProfile profile = controlFlowProfiles.computeIfAbsent(
+              context.getInstrumentedSourceSection(), src -> new BranchProfile(src));
+          return new ControlFlowProfileNode(profile);
         });
   }
 
