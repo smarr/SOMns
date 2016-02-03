5e666176a85ebe8ab8ac8791354287beb3163308
Stefan Marr <git@stefan-marr.de>
2016-02-03 01:05:22 +0100

    Added toJson to profiles
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/dym/profiles/BranchProfile.java
+++ src/dym/profiles/BranchProfile.java
@@ -1,9 +1,14 @@
 package dym.profiles;
 
 import com.oracle.truffle.api.source.SourceSection;
+import com.oracle.truffle.api.utilities.JSONHelper;
+import com.oracle.truffle.api.utilities.JSONHelper.JSONObjectBuilder;
+import com.oracle.truffle.api.utilities.JSONHelper.JSONStringBuilder;
+
+import dym.JsonSerializable;
 
 
-public class BranchProfile {
+public class BranchProfile implements JsonSerializable {
   private final SourceSection source;
   private long trueCount;
   private long falseCount;
@@ -20,5 +25,13 @@ public class BranchProfile {
       falseCount += 1;
       assert falseCount > 0 : "TODO: handle overflow, by calculating a ratio";
     }
+  }
+
+  @Override
+  public JSONStringBuilder toJson() {
+    JSONObjectBuilder result = JSONHelper.object();
+    result.add("trueCount",  trueCount);
+    result.add("falseCount", falseCount);
+    return result;
   }
 }
--- src/dym/profiles/Counter.java
+++ src/dym/profiles/Counter.java
@@ -1,13 +1,22 @@
 package dym.profiles;
 
 import com.oracle.truffle.api.source.SourceSection;
+import com.oracle.truffle.api.utilities.JSONHelper;
+import com.oracle.truffle.api.utilities.JSONHelper.JSONObjectBuilder;
+import com.oracle.truffle.api.utilities.JSONHelper.JSONStringBuilder;
 
-public class Counter {
+import dym.JsonSerializable;
+
+public class Counter implements JsonSerializable {
   private final SourceSection source;
   private int invocationCount;
 
   public Counter(final SourceSection source) {
     this.source = source;
+  }
+
+  public SourceSection getSourceSection() {
+    return source;
   }
 
   public void inc() {
@@ -21,5 +30,12 @@ public class Counter {
   @Override
   public String toString() {
     return "Cnt[" + invocationCount + ", " + source.getIdentifier() + "]";
+  }
+
+  @Override
+  public JSONStringBuilder toJson() {
+    JSONObjectBuilder result = JSONHelper.object();
+    result.add("count", invocationCount);
+    return result;
   }
 }
