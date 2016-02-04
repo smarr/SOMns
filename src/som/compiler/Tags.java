b2db27018c2103615a83be71ecd5b79bef2c9b75
Stefan Marr <git@stefan-marr.de>
2016-02-04 16:06:26 +0100

    Added more tags
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/compiler/Tags.java
+++ src/som/compiler/Tags.java
@@ -12,6 +12,11 @@ public class Tags {
 
   public static final String FIELD_READ         = "FIELD_READ";
   public static final String FIELD_WRITE        = "FIELD_WRITE";
+
+  public static final String LOCAL_VAR_READ     = "LOCAL_VAR_READ";
+  public static final String LOCAL_VAR_WRITE    = "LOCAL_VAR_WRITE";
+  public static final String LOCAL_ARG_READ     = "LOCAL_ARG_READ";
+
   public static final String ARRAY_READ         = "ARRAY_READ";
   public static final String ARRAY_WRITE        = "ARRAY_WRITE";
   public static final String LOOP_BODY          = "LOOP_BODY";
@@ -21,4 +26,6 @@ public class Tags {
   public static final String SYNTAX_LITERAL = "SYNTAX_LITERAL";
   public static final String SYNTAX_COMMENT = "SYNTAX_COMMENT";
   public static final String SYNTAX_IDENTIFIER = "SYNTAX_IDENTIFIER";
+  public static final String SYNTAX_ARGUMENT = "SYNTAX_ARGUMENT";
+  public static final String SYNTAX_LOCAL_VARIABLE = "SYNTAX_LOCAL_VARIABLE";
 }
