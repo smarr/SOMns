1c35367ebdb3c53df4290ac256348a268ab4b549
Stefan Marr <git@stefan-marr.de>
2016-02-12 18:48:40 +0100

    Revert addition of instrumentation related tests
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/som/tests/SomPolyglotTests.java
+++ tests/som/tests/SomPolyglotTests.java
@@ -1,6 +1,5 @@
 package som.tests;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -14,9 +13,7 @@ import som.interpreter.SomLanguage;
 
 import com.oracle.truffle.api.vm.PolyglotEngine;
 import com.oracle.truffle.api.vm.PolyglotEngine.Builder;
-import com.oracle.truffle.api.vm.PolyglotEngine.Instrument;
 import com.oracle.truffle.api.vm.PolyglotEngine.Value;
-import com.oracle.truffle.tools.TruffleProfiler;
 
 
 public class SomPolyglotTests {
@@ -40,36 +37,6 @@ public class SomPolyglotTests {
     builder.config(SomLanguage.MIME_TYPE, SomLanguage.CMD_ARGS, args);
     PolyglotEngine vm = builder.build();
     Value result = vm.eval(SomLanguage.START);
-    assertNotNull(result);
-  }
-
-  @Test
-  public void executeHelloWorldWithTruffleProfiler() throws IOException {
-    String[] args = new String[] {"core-lib/Hello.som"};
-
-    Builder builder = PolyglotEngine.newBuilder();
-    builder.config(SomLanguage.MIME_TYPE, SomLanguage.CMD_ARGS, args);
-    PolyglotEngine vm = builder.build();
-    Instrument profiler = vm.getInstruments().get(TruffleProfiler.ID);
-    assertTrue(profiler.isEnabled());
-    Value result = vm.eval(SomLanguage.START);
-    assertTrue(profiler.isEnabled());
-    vm.dispose();
-    assertNotNull(result);
-  }
-
-  @Test
-  public void executeHelloWorldWithoutTruffleProfiler() throws IOException {
-    String[] args = new String[] {"core-lib/Hello.som"};
-
-    Builder builder = PolyglotEngine.newBuilder();
-    builder.config(SomLanguage.MIME_TYPE, SomLanguage.CMD_ARGS, args);
-    PolyglotEngine vm = builder.build();
-    Instrument profiler = vm.getInstruments().get(TruffleProfiler.ID);
-    assertTrue(profiler.isEnabled());
-    profiler.setEnabled(false);
-    Value result = vm.eval(SomLanguage.START);
-    assertFalse(profiler.isEnabled());
     assertNotNull(result);
   }
 }
