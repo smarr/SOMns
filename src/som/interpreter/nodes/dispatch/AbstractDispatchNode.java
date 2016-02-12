d65d6bd84a2bc3b425e8f0b882593f992a4c5224
Stefan Marr <git@stefan-marr.de>
2016-02-12 18:48:25 +0100

    Revert making AbstractDispatchNode instrumentable
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/dispatch/AbstractDispatchNode.java
+++ src/som/interpreter/nodes/dispatch/AbstractDispatchNode.java
@@ -1,14 +1,10 @@
 package som.interpreter.nodes.dispatch;
 
-import som.instrumentation.DispatchNodeWrapper;
-
 import com.oracle.truffle.api.frame.VirtualFrame;
-import com.oracle.truffle.api.instrumentation.Instrumentable;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.source.SourceSection;
 
 
-@Instrumentable(factory = DispatchNodeWrapper.class)
 public abstract class AbstractDispatchNode
     extends Node implements DispatchChain {
   public static final int INLINE_CACHE_SIZE = 6;
