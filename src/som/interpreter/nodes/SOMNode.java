056b82a4f3f429e9703fb35b12ff05cb60bc3ef1
Stefan Marr <git@stefan-marr.de>
2016-03-29 16:09:24 +0200

    Wrapper want to delegate getSourceSection(), so, this can't be final
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/SOMNode.java
+++ src/som/interpreter/nodes/SOMNode.java
@@ -45,7 +45,7 @@ public abstract class SOMNode extends Node {
   }
 
   @Override
-  public final SourceSection getSourceSection() {
+  public SourceSection getSourceSection() {
     return sourceSection;
   }
 
