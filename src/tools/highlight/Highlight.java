a164b133430fbbb8a529fabdf34ba7857461472e
Stefan Marr <git@stefan-marr.de>
2016-03-30 22:05:31 +0200

    Added comments to Highlight tool
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/tools/highlight/Highlight.java
+++ src/tools/highlight/Highlight.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright (c) 2016 Stefan Marr
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package tools.highlight;
 
 import java.util.HashMap;
@@ -11,6 +32,21 @@ import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.api.source.SourceSection;
 
 
+/**
+ * <p>
+ * The Highlight tool generates a JSON file with the source code that was
+ * executed by the polyglot engine and uses {@link Tags} to provide information
+ * for syntax highlighting based on source sections.
+ *
+ * <p>
+ * Highlight expects that the root nodes that should be included are reported
+ * to it using the {@link reportParsedRootNode} function.
+ * For elements that are not part of the AST, the {@link reportNonAstSyntax}
+ * function can be used.
+ *
+ * <p>
+ * The output file can be configured by the Java property: hl.output
+ */
 @Registration(id = Highlight.ID)
 public final class Highlight extends TruffleInstrument {
 
@@ -19,6 +55,14 @@ public final class Highlight extends TruffleInstrument {
   private static final Map<SourceSection, Set<Class<? extends Tags>>> nonAstSourceSections = new HashMap<>();
   private static final Set<RootNode> rootNodes = new HashSet<>();
 
+  /**
+   * Report an element for highlighting that is not part of an AST,
+   * and thus is not known to this tool from a root node
+   * ({@see reportParsedRootNode}).
+   *
+   * @param tag, indicating the type of the element
+   * @param source, the source section describing the location of the element
+   */
   // TODO: this is a bad hack. but, I don't know how to work around the polyglot engine otherwise
   //       normally, each polyglot engine should have a separate map
   public static void reportNonAstSyntax(final Class<? extends Tags> tag, final SourceSection source) {
@@ -26,12 +70,15 @@ public final class Highlight extends TruffleInstrument {
     set.add(tag);
   }
 
-  // TODO: this is a bad hack. but, I don't know how to work around the polyglot engine otherwise
-  //       normally, each polyglot engine should have a separate map
   /**
+   * Report a root node of which the AST nodes should be used for highlighting
+   * code.
+   *
    * @param rootNode, created by the parser.
    *         While we would like to have all, there is not simply way to get them.
    */
+  // TODO: this is a bad hack. but, I don't know how to work around the polyglot engine otherwise
+  //       normally, each polyglot engine should have a separate map
   public static void reportParsedRootNode(final RootNode rootNode) {
     rootNodes.add(rootNode);
   }
--- src/tools/highlight/Tags.java
+++ src/tools/highlight/Tags.java
@@ -1,41 +1,115 @@
+/**
+ * Copyright (c) 2016 Stefan Marr
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package tools.highlight;
+
+import com.oracle.truffle.api.source.SourceSection;
 
 
 public abstract class Tags {
   private Tags() { }
 
+  public static Class<?>[] getAll() {
+    return new Class<?>[] {KeywordTag.class, LiteralTag.class, CommentTag.class,
+        IdentifierTag.class, ArgumentTag.class, LocalVariableTag.class,
+        StatementSeparatorTag.class, DelimiterOpeningTag.class,
+        DelimiterClosingTag.class};
+  }
+
+  /**
+   * Marks keywords or keyword-like language constructs.
+   * Elements that are not preserved in the AST should reported via
+   * {@link Highlight#reportNonAstSyntax(Class, SourceSection)}.
+   */
   public final class KeywordTag extends Tags {
     private KeywordTag() { }
   }
 
+  /**
+   * Marks literal values.
+   */
   public final class LiteralTag extends Tags {
     private LiteralTag() { }
   }
 
+  /**
+   * Marks comments.
+   * Elements that are not preserved in the AST should reported via
+   * {@link Highlight#reportNonAstSyntax(Class, SourceSection)}.
+   */
   public final class CommentTag extends Tags {
     private CommentTag() { }
   }
 
+  /**
+   * Marks identifiers. Currently only used for class names.
+   * TODO: figure out whether this is really useful.
+   * Elements that are not preserved in the AST should reported via
+   * {@link Highlight#reportNonAstSyntax(Class, SourceSection)}.
+   */
   public final class IdentifierTag extends Tags {
     private IdentifierTag() { }
   }
 
+  /**
+   * Marks formal and actual arguments.
+   * Formal arguments that are not preserved in the AST should reported via
+   * {@link Highlight#reportNonAstSyntax(Class, SourceSection)}.
+   */
   public final class ArgumentTag extends Tags {
     private ArgumentTag() { }
   }
 
+  /**
+   * Marks local variables.
+   * Variable declarations that are not part of the AST should be reported via
+   * {@link Highlight#reportNonAstSyntax(Class, SourceSection)}.
+   */
   public final class LocalVariableTag extends Tags {
     private LocalVariableTag() { }
   }
 
+  /**
+   * Marks line endings like ';', or '.'.
+   * Elements that are not preserved in the AST should reported via
+   * {@link Highlight#reportNonAstSyntax(Class, SourceSection)}.
+   */
   public final class StatementSeparatorTag extends Tags {
     private StatementSeparatorTag() { }
   }
 
+  /**
+   * Marks opening delimiters such as parentheses.
+   * Elements that are not preserved in the AST should reported via
+   * {@link Highlight#reportNonAstSyntax(Class, SourceSection)}.
+   */
   public final class DelimiterOpeningTag extends Tags {
     private DelimiterOpeningTag() { }
   }
 
+  /**
+   * Marks closing delimiters such as parentheses.
+   * Elements that are not preserved in the AST should reported via
+   * {@link Highlight#reportNonAstSyntax(Class, SourceSection)}.
+   */
   public final class DelimiterClosingTag extends Tags {
     private DelimiterClosingTag() { }
   }
