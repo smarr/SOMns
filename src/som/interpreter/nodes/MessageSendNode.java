5f399a77547e43db63748f16075b4d7d6e59e8b7
Stefan Marr <git@stefan-marr.de>
2016-04-14 21:51:02 +0200

    Added Debugger's CallTag to generic message sends
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/MessageSendNode.java
+++ src/som/interpreter/nodes/MessageSendNode.java
@@ -7,6 +7,7 @@ import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.instrumentation.Instrumentable;
 import com.oracle.truffle.api.instrumentation.StandardTags;
+import com.oracle.truffle.api.instrumentation.StandardTags.CallTag;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.NodeCost;
@@ -758,6 +759,8 @@ public final class MessageSendNode {
     @Override
     protected boolean isTaggedWith(final Class<?> tag) {
       if (tag == VirtualInvoke.class) {
+        return true;
+      } else if (tag == CallTag.class) {
         return true;
       } else {
         return super.isTaggedWith(tag);
