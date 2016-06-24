f8868a13de31c97432169463e005dc0d885e12d5
Stefan Marr <git@stefan-marr.de>
2016-06-24 18:23:53 +0200

    Mark String hash methods as boundary
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/HashPrim.java
+++ src/som/primitives/HashPrim.java
@@ -1,5 +1,6 @@
 package som.primitives;
 
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.source.SourceSection;
@@ -15,11 +16,13 @@ public abstract class HashPrim extends UnaryExpressionNode {
   public HashPrim(final SourceSection source) { super(false, source); }
 
   @Specialization
+  @TruffleBoundary
   public final long doString(final String receiver) {
     return receiver.hashCode();
   }
 
   @Specialization
+  @TruffleBoundary
   public final long doSSymbol(final SSymbol receiver) {
     return receiver.getString().hashCode();
   }
