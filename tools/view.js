50aa519fa7e4269405d0d1345d101815c9f829b2
Stefan Marr <git@stefan-marr.de>
2016-07-29 17:07:40 +0200

    Remove histogram plot for access/exec? metrics
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tools/view.js
+++ tools/view.js
@@ -251,70 +251,6 @@ function showAccessProfile(profileData, rootSpan) {
   accessProfiles.push(profileData);
 }
 
-function showHistogramOfAccess() {
-  var numBins = 6,
-    binSize = Math.ceil(Math.log(maxAccessCount) / numBins),
-    bins = new Array(numBins);
-  bins.fill(0);
-
-  accessProfiles.forEach(function(profileData) {
-    var bin = Math.floor(Math.log(profileData.count) / binSize);
-    bins[bin] += 1;
-    $("#" + profileData.spanId).addClass("bucket-" + bin);
-  });
-
-  // generateGraph();
-}
-
-function generateGraph() {
-  // A formatter for counts.
-  var formatCount = d3.format(",.0f");
-  var margin = {top: 10, right: 30, bottom: 30, left: 30},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-  var x = d3.scale.linear()
-    .domain([0, d3.max(accessProfiles, function(profileData) { return profileData.count; })])
-    .range([0, width]);
-  // Generate a histogram using 10 uniformly-spaced bins.
-  var hist = d3.layout.histogram()
-    .bins(10)
-    .value(function(profileData) { return profileData.count; });
-  var data = hist(accessProfiles);
-
-  var y = d3.scale.linear()
-    .domain([0, d3.max(data, function(d) { return d.y; })])
-    .range([height, 0]);
-
-  var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-  var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  var bar = svg.selectAll(".bar")
-    .data(data)
-    .enter().append("g")
-    .attr("class", "bar")
-    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
-  bar.append("rect")
-    .attr("x", 1)
-    .attr("width", x(data[0].dx) - 1)
-    .attr("height", function(d) { return height - y(d.y); });
-  bar.append("text")
-    .attr("dy", ".75em")
-    .attr("y", 6)
-    .attr("x", x(data[0].dx) / 2)
-    .attr("text-anchor", "middle")
-    .text(function(d) { return formatCount(d.y); });
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-}
 /**
  * The HTML View, which realizes all access to the DOM for displaying
  * data and reacting to events.
@@ -343,8 +279,6 @@ View.prototype.displaySources = function (msg) {
   for (var ssId in msg.sections) {
     this.showSectionData(msg.sections[ssId]);
   }
-
-  showHistogramOfAccess();
 
   $('.nav-tabs a[href="#' + sId + '"]').tab('show');
 
