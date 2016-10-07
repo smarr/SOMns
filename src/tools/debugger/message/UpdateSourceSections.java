29b5ba4bd2d1c46e61484afa2c33a9fb2fa6f5e8
Stefan Marr <git@stefan-marr.de>
2016-10-07 14:12:24 +0200

    Added UpdateSourceSection, work in progress
    This is not yet supported by the front-end, but is necessary, since I move the updating of source section info out of the suspended event message
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/tools/debugger/message/UpdateSourceSections.java
@@ -0,0 +1,32 @@
+package tools.debugger.message;
+
+import tools.SourceCoordinate.TaggedSourceCoordinate;
+
+/**
+ * This message is send to update the front-end with the latest source section
+ * information.
+ *
+ * <p>
+ * This is useful to have dynamic semantic highlighting for a dynamic language.
+ * The highlighting can then also indicate field reads done by slot getters,
+ * distinguish local accesses from message sends in a uniform language, etc.
+ */
+@SuppressWarnings("unused")
+public class UpdateSourceSections extends Message {
+  private final SourceInfo[] updates;
+
+  public UpdateSourceSections(final SourceInfo[] updates) {
+    this.updates = updates;
+  }
+
+  public static class SourceInfo {
+    private final String sourceUri;
+    private final TaggedSourceCoordinate[] sections;
+
+    public SourceInfo(final String sourceUri,
+        final TaggedSourceCoordinate[] sections) {
+      this.sourceUri = sourceUri;
+      this.sections  = sections;
+    }
+  }
+}
