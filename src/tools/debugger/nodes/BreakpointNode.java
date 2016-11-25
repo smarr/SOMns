2a2a454e81797ad304929622e32da0a96d93d297
Stefan Marr <git@stefan-marr.de>
2016-11-25 10:47:06 +0100

    Fix whitespace
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/tools/debugger/nodes/BreakpointNode.java
+++ src/tools/debugger/nodes/BreakpointNode.java
@@ -11,22 +11,21 @@ import tools.debugger.session.BreakpointEnabling;
  * It has two possible states, enable or disable.
  */
 public abstract class BreakpointNode extends AbstractBreakpointNode {
-    protected final BreakpointEnabling<?> breakpoint;
+  protected final BreakpointEnabling<?> breakpoint;
 
-    protected BreakpointNode(final BreakpointEnabling<?> breakpoint) {
-      this.breakpoint = breakpoint;
-    }
-
-    @Specialization(assumptions = "bpUnchanged", guards = "breakpoint.isDisabled()")
-    public final boolean breakpointDisabled(
-        @Cached("breakpoint.getAssumption()") final Assumption bpUnchanged) {
-      return false;
-    }
-
-    @Specialization(assumptions = {"bpUnchanged"}, guards = {"breakpoint.isEnabled()"})
-    public final boolean breakpointEnabled(
-        @Cached("breakpoint.getAssumption()") final Assumption bpUnchanged) {
-      return true;
-    }
+  protected BreakpointNode(final BreakpointEnabling<?> breakpoint) {
+    this.breakpoint = breakpoint;
   }
 
+  @Specialization(assumptions = "bpUnchanged", guards = "breakpoint.isDisabled()")
+  public final boolean breakpointDisabled(
+      @Cached("breakpoint.getAssumption()") final Assumption bpUnchanged) {
+    return false;
+  }
+
+  @Specialization(assumptions = {"bpUnchanged"}, guards = {"breakpoint.isEnabled()"})
+  public final boolean breakpointEnabled(
+      @Cached("breakpoint.getAssumption()") final Assumption bpUnchanged) {
+    return true;
+  }
+}
