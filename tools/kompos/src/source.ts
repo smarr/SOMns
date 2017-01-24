d04450acc15572c62994d52b3596e3a41d5f053b
Stefan Marr <git@stefan-marr.de>
2017-01-24 13:24:59 +0100

    Remove dead code from source.ts
    Dummy InvocationProfile, CountingProfile, MessageHistory, etc.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tools/kompos/src/source.ts
+++ tools/kompos/src/source.ts
@@ -1,43 +1,6 @@
 /* jshint -W097 */
 "use strict";
 
-
-/**
- * Dummy definition for IDE support. These objects are
- * generally read from the web socket connection.
- * @constructor
- */
-function InvocationProfile() {
-  this.invocations = 0;
-  this.somTypes = [];
-}
-
-/**
- * Dummy definition for IDE support. These objects are
- * generally read from the web socket connection.
- * @constructor
- */
-function CountingProfile() {
-  this.count = 0;
-}
-
-function Actor(id, name, typeName) {
-  this.id       = id;
-  this.name     = name;
-  this.typeName = typeName;
-}
-
-function MessageHistory(messages, actors) {
-  this.messages = messages; // per actor
-  this.actors   = actors;
-}
-
-function Message(id, sender, receiver) {
-  this.id       = id;
-  this.sender   = sender;
-  this.receiver = receiver;
-}
-
 export function dbgLog(msg) {
   var tzOffset = (new Date()).getTimezoneOffset() * 60000; // offset in milliseconds
   var localISOTime = (new Date(Date.now() - tzOffset)).toISOString().slice(0,-1);
