013383551aab8a6214fa5d4428fd77603f370a15
Stefan Marr <git@stefan-marr.de>
2017-03-15 17:42:54 +0100

    Fix timestamp tracking for messages
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/actors/Actor.java
+++ src/som/interpreter/actors/Actor.java
@@ -246,6 +246,7 @@ public class Actor implements Activity {
           int i = 0;
           for (EventualMessage msg : mailboxExtension) {
             execute(msg, currentThread, dbg, i);
+            i++;
           }
         }
       } finally {
@@ -257,13 +258,12 @@ public class Actor implements Activity {
     }
 
     private void execute(final EventualMessage msg,
-        final ActorProcessingThread currentThread, final WebDebugger dbg, int i) {
+        final ActorProcessingThread currentThread, final WebDebugger dbg, final int i) {
       currentThread.currentMessage = msg;
       handleBreakPoints(msg, dbg);
 
       if (i >= 0 && VmSettings.MESSAGE_TIMESTAMPS) {
         executionTimeStamps[i] = System.currentTimeMillis();
-        i++;
       }
 
       try {
