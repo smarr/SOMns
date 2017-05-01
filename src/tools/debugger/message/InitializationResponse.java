9e3279ff4c3001416c96cd4d7b7d88aa168e99c6
Stefan Marr <git@stefan-marr.de>
2017-05-01 10:00:28 +0200

    Added tagsToStrings() helper to reduce code duplication
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/tools/debugger/message/InitializationResponse.java
+++ src/tools/debugger/message/InitializationResponse.java
@@ -1,5 +1,6 @@
 package tools.debugger.message;
 
+import tools.concurrency.Tags;
 import tools.debugger.entities.ActivityType;
 import tools.debugger.entities.BreakpointType;
 import tools.debugger.entities.EntityType;
@@ -29,6 +30,19 @@ public final class InitializationResponse extends OutgoingMessage {
     }
   }
 
+  private static String[] tagsToStrings(final Class<? extends Tags>[] tags) {
+    if (tags == null) {
+      return null;
+    }
+
+    String[] result = new String[tags.length];
+
+    for (int i = 0; i < tags.length; i += 1) {
+      result[i] = tags[i].getSimpleName();
+    }
+    return result;
+  }
+
   private static final class BreakpointData {
     private final String name;
     private final String label;
@@ -37,12 +51,7 @@ public final class InitializationResponse extends OutgoingMessage {
     private BreakpointData(final BreakpointType bp) {
       this.name = bp.name;
       this.label = bp.label;
-
-      this.applicableTo = new String[bp.applicableTo.length];
-
-      for (int i = 0; i < bp.applicableTo.length; i += 1) {
-        applicableTo[i] = bp.applicableTo[i].getSimpleName();
-      }
+      this.applicableTo = tagsToStrings(bp.applicableTo);
     }
   }
 
@@ -58,16 +67,7 @@ public final class InitializationResponse extends OutgoingMessage {
       this.label = type.label;
       this.group = type.group.label;
       this.icon  = type.icon;
-
-      if (type.applicableTo == null) {
-          this.applicableTo = null;
-      } else {
-        this.applicableTo = new String[type.applicableTo.length];
-
-        for (int i = 0; i < type.applicableTo.length; i += 1) {
-          applicableTo[i] = type.applicableTo[i].getSimpleName();
-        }
-      }
+      this.applicableTo = tagsToStrings(type.applicableTo);
     }
   }
 
