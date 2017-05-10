82083045f51373a04287b0dd340a40e30fd3c7cf
Stefan Marr <git@stefan-marr.de>
2017-05-10 13:03:42 +0200

    [Fix] timestamp removal
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/tools/concurrency/ActorExecutionTrace.java
+++ src/tools/concurrency/ActorExecutionTrace.java
@@ -106,10 +106,6 @@ public class ActorExecutionTrace {
 
     byte eventid = TraceData.MESSAGE_BIT;
 
-    if (VmSettings.MESSAGE_TIMESTAMPS) {
-      eventid |= TraceData.TIMESTAMP_BIT;
-    }
-
     if (VmSettings.MESSAGE_PARAMETERS) {
       eventid |= TraceData.PARAMETER_BIT;
     }
