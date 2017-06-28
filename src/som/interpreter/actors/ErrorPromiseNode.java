3cc10cabb7d820796074d9437fdc9b8f39750fc3
Stefan Marr <git@stefan-marr.de>
2017-06-28 18:04:50 +0200

    Add missing breakpoint triggering to promise>>#error:
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/actors/ErrorPromiseNode.java
+++ src/som/interpreter/actors/ErrorPromiseNode.java
@@ -2,6 +2,7 @@ package som.interpreter.actors;
 
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.source.SourceSection;
 
 import som.VM;
@@ -21,8 +22,14 @@ public abstract class ErrorPromiseNode extends AbstractPromiseResolutionNode {
    * Standard error case, when the promise is errored with a value that's not a promise.
    */
   @Specialization(guards = {"notAPromise(result)"})
-  public SResolver standardError(final SResolver resolver, final Object result,
-      final boolean haltOnResolver, final boolean haltOnResolution) {
+  public SResolver standardError(final VirtualFrame frame, final SResolver resolver,
+      final Object result, final boolean haltOnResolver, final boolean haltOnResolution) {
+    SPromise promise = resolver.getPromise();
+
+    if (haltOnResolver || promise.getHaltOnResolver()) {
+      haltNode.executeEvaluated(frame, result);
+    }
+
     resolvePromise(Resolution.ERRONEOUS, resolver, result, haltOnResolution);
     return resolver;
   }
