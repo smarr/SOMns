ad87eb02f1eca14c41be41c9db9197c0540a7401
Richard Roberts <rykardo.r@gmail.com>
2017-08-03 11:04:17 +1200

    New object literal node and adapted SClass
    The object literal is created with a mixin definiton that corresponds to
    this object's implicitally declared class. This new node is responsible
    for first instantiating the implicit class and then the object from
    that class.
    
    A context field has also been added to the `SClass`, which is responsible for
    holding the activation in which the object literal is created. Methods have
    been added to `MixinDefintion` and `ClassInstantiationNode` to pass the the
    `MaterializedFrame` as an argument to the instantiation.

---
+++ src/som/interpreter/nodes/literals/ObjectLiteralNode.java
@@ -0,0 +1,50 @@
+package som.interpreter.nodes.literals;
+
+import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.source.SourceSection;
+
+import som.compiler.MixinDefinition;
+import som.interpreter.nodes.ExpressionNode;
+import som.interpreter.nodes.InstantiationNode.ObjectLiteralInstantiationNode;
+import som.interpreter.nodes.InstantiationNodeFactory.ObjectLiteralInstantiationNodeGen;
+import som.interpreter.nodes.MessageSendNode.AbstractMessageSendNode;
+import som.vmobjects.SClass;
+import som.vmobjects.SObjectWithClass;
+
+
+public class ObjectLiteralNode extends LiteralNode {
+
+  private MixinDefinition mixinDef;
+  private RootCallTarget superClassResolver;
+
+  @Child private ExpressionNode outerRead;
+  @Child private AbstractMessageSendNode newMessage;
+  @Child private ObjectLiteralInstantiationNode instantiation;
+
+  /**
+   * An object literal node. When invoked this node will instantiate a new object literal class
+   * (defined by this node's `MixinDefinition`) that has the current activation as its enclosing
+   * scope. Once this class has been created, this node will instantiate and return a new object
+   * from that class.
+   */
+  public ObjectLiteralNode(final MixinDefinition mixinDefiniton, final ExpressionNode outerRead,
+      final ExpressionNode newMessage, final SourceSection source) {
+    super(source);
+    this.mixinDef = mixinDefiniton;
+    this.superClassResolver = mixinDefiniton.getSuperclassAndMixinResolutionInvokable().createCallTarget();
+
+    this.outerRead = outerRead;
+    this.newMessage = (AbstractMessageSendNode) newMessage;
+    this.instantiation = ObjectLiteralInstantiationNodeGen.create(mixinDefiniton);
+  }
+
+  @Override
+  public Object executeGeneric(final VirtualFrame frame) {
+    SObjectWithClass outer = (SObjectWithClass) outerRead.executeGeneric(frame);
+    Object superclassAndMixins = superClassResolver.call(outer);
+    SClass sClassObject = instantiation.execute(outer, superclassAndMixins, frame.materialize());
+    return newMessage.doPreEvaluated(frame, new Object[] {sClassObject});
+  }
+
+}
