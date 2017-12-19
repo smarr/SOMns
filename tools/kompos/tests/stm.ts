fb4e3f5ee9ef0b29d31469191d6a25ddbf892ebd
Stefan Marr <git@stefan-marr.de>
2017-12-19 09:55:22 +0100

    Replace ad hoc approach for STM tests with stepping DSL
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tools/kompos/tests/stm.ts
+++ tools/kompos/tests/stm.ts
@@ -1,111 +1,143 @@
 import { expect } from "chai";
 import { resolve } from "path";
 
-import { BreakpointType, SteppingType } from "./somns-support";
-import { TestConnection, HandleStoppedAndGetStackTrace, expectStack } from "./test-setup";
-import { createSectionBreakpointData, StackTraceResponse } from "../src/messages";
+import { BreakpointType as BT, SteppingType as ST } from "./somns-support";
+import { TestConnection, TestController } from "./test-setup";
+import { createSectionBreakpointData, createLineBreakpointData } from "../src/messages";
+import { Test, expectStops } from "./stepping";
 
 const STM_FILE = resolve("tests/stm.ns");
 const STM_URI = "file:" + STM_FILE;
 
 describe("Setting STM Breakpoints", () => {
   let conn: TestConnection;
-  let ctrl: HandleStoppedAndGetStackTrace;
 
   const closeConnectionAfterSuite = (done) => {
     conn.fullyConnected.then(_ => { conn.close(done); });
     conn.fullyConnected.catch(reason => done(reason));
   };
 
-  before("Start SOMns and Connect", () => {
-    const beforeBp = createSectionBreakpointData(STM_URI, 11, 8, 74,
-      BreakpointType.ATOMIC_BEFORE, true);
-    const commitBp = createSectionBreakpointData(STM_URI, 11, 8, 74,
-      BreakpointType.ATOMIC_BEFORE_COMMIT, true);
-    conn = new TestConnection(null, null, STM_FILE);
-    ctrl = new HandleStoppedAndGetStackTrace(
-      [beforeBp, commitBp], conn, conn.fullyConnected, 6);
-  });
-
-  after(closeConnectionAfterSuite);
-
-  const thread1 = [
-    { s: 6, n: "STM>>#doCount:", l: 11 },
-    // line 13 is ok, because the bp uses source section of whole block
-    { s: 7, n: "STM>>#λdoCount@11@16", l: 13 },
-    { s: 6, n: "STM>>#doCount:", l: 17 },
+  const steppingTests: Test[] = [
+    {
+      title: "stepping to message receiver on same actor",
+      test: STM_FILE,
+      initialBreakpoints: [
+        createLineBreakpointData(STM_URI, 39, true),
+        createSectionBreakpointData(STM_URI, 11, 8, 74, BT.ATOMIC_BEFORE, true),
+        createSectionBreakpointData(STM_URI, 11, 8, 74, BT.ATOMIC_BEFORE_COMMIT, true)],
+      initialStop: {
+        line: 39,
+        methodName: "STM>>#main:",
+        stackHeight: 5,
+        activity: "main"
+      },
+      steps: [
+        {
+          type: ST.RESUME,
+          activity: "main",
+          stops: [{
+            line: 11,
+            methodName: "STM>>#doCount:",
+            stackHeight: 6,
+            activity: "main"
+          },
+          {
+            line: 11,
+            methodName: "STM>>#doCount:",
+            stackHeight: 2,
+            activity: "thread2"
+          }]
+        },
+        {
+          type: ST.RESUME,
+          activity: "main",
+          stops: [{
+            line: 13,
+            methodName: "STM>>#λdoCount@11@16",
+            stackHeight: 7,
+            activity: "main"
+          }]
+        },
+        {
+          type: ST.RESUME,
+          activity: "thread2",
+          stops: [{
+            line: 13,
+            methodName: "STM>>#λdoCount@11@16",
+            stackHeight: 3,
+            activity: "thread2"
+          }]
+        },
+        {
+          type: ST.STEP_OVER,
+          activity: "main",
+          stops: [{
+            line: 17,
+            methodName: "STM>>#doCount:",
+            stackHeight: 6,
+            activity: "main"
+          }]
+        },
+        {
+          type: ST.RESUME,
+          activity: "thread2",
+          stops: [{
+            line: 13,
+            methodName: "STM>>#λdoCount@11@16",
+            stackHeight: 3,
+            activity: "thread2"
+          }]
+        }
+      ]
+    }
   ];
 
-  const thread2 = [
-    { s: 2, n: "STM>>#doCount:", l: 11 },
-    { s: 3, n: "STM>>#λdoCount@11@16", l: 13 },
-    { s: 3, n: "STM>>#λdoCount@11@16", l: 13 },
-  ];
+  for (const suite of steppingTests) {
+    if (suite.skip) {
+      describe.skip(suite.title, () => {
+        if (!suite.steps) { return; }
+        suite.steps.forEach(step => {
+          const desc = step.desc ? step.desc : `do ${step.type} on ${step.activity}.`;
+          it.skip(desc, () => { });
+        });
+      });
+      continue;
+    }
 
-  let actId = 0;
-  it("should break on #atomic, 1st time, and resume", () => {
-    return ctrl.stackPs[0].then(msg => {
-      actId = msg.activityId;
-      conn.sendDebuggerAction(SteppingType.RESUME, ctrl.stoppedActivities[msg.requestId]);
+    describe(suite.title, () => {
+      let ctrl: TestController;
 
-      const exp = (msg.activityId === 0 ? thread1 : thread2)[0];
-      return expectStack(msg.stackFrames, exp.s, exp.n, exp.l);
+      before("Start SOMns and Connect", () => {
+        const arg = suite.testArg ? [suite.testArg] : null;
+        conn = new TestConnection(arg, false, suite.test);
+        ctrl = new TestController(suite, conn, conn.fullyConnected);
+      });
+
+      after(closeConnectionAfterSuite);
+
+      it("should stop initially at breakpoint", () => {
+        return ctrl.stopsDoneForStep.then(stops => {
+          expect(stops).has.lengthOf(1);
+          expect(stops[0]).to.deep.equal(suite.initialStop);
+        });
+      });
+
+
+      describe("should", () => {
+        if (!suite.steps) { return; }
+
+        suite.steps.forEach(step => {
+          const expectedStops = step.stops ? step.stops.length : 0;
+          const desc = step.desc ? step.desc : `do ${step.type} on ${step.activity} and stop ${expectedStops} times.`;
+          it(desc, () => {
+            const stopPs = ctrl.doNextStep(step.type, step.activity, step.stops);
+
+            return stopPs.then(allStops => {
+              expectStops(allStops, step.stops);
+            });
+          });
+        });
+      });
     });
-  });
-
-  it("should break on #atomic, 2nd time, and resume", () => {
-    return ctrl.stackPs[1].then(msg => {
-      expect(msg.activityId).not.equal(actId);
-      conn.sendDebuggerAction(SteppingType.RESUME, ctrl.stoppedActivities[msg.requestId]);
-
-      const exp = (msg.activityId === 0 ? thread1 : thread2)[0];
-      return expectStack(msg.stackFrames, exp.s, exp.n, exp.l);
-    });
-  });
-
-  let postponedAct = null;
-
-  it("should stop before commit, two times, and single step", () => {
-    const handler = function(msg: StackTraceResponse) {
-      if (msg.activityId === 0) {
-        conn.sendDebuggerAction(SteppingType.STEP_INTO, ctrl.stoppedActivities[msg.requestId]);
-      } else {
-        postponedAct = ctrl.stoppedActivities[msg.requestId];
-      }
-
-      const exp = (msg.activityId === 0 ? thread1 : thread2)[1];
-      return expectStack(msg.stackFrames, exp.s, exp.n, exp.l);
-    };
-
-    const p1 = ctrl.stackPs[2].then(msg => {
-      return handler(msg);
-    });
-    const p2 = ctrl.stackPs[3].then(msg => {
-      return handler(msg);
-    });
-    return Promise.all([p1, p2]);
-  });
-
-  it("1st thread should stop before next operation, 2nd thread should retry transaction", () => {
-    const handle = function(msg: StackTraceResponse) {
-      if (msg.activityId === 0) {
-        console.assert(postponedAct !== null);
-        conn.sendDebuggerAction(SteppingType.STEP_INTO, postponedAct);
-
-        const exp = thread1[2];
-        return expectStack(msg.stackFrames, exp.s, exp.n, exp.l);
-      } else {
-        const exp = thread2[2];
-        return expectStack(msg.stackFrames, exp.s, exp.n, exp.l);
-      }
-    };
-
-    const p1 = ctrl.stackPs[4].then(msg => {
-      return handle(msg);
-    });
-    const p2 = ctrl.stackPs[5].then(msg => {
-      return handle(msg);
-    });
-    return Promise.all([p1, p2]);
-  });
+  }
 });
