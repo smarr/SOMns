cbebe969dc8fec5210c09dcbbf5dac7771a93042
Dominik Aumayr <daumayr162@gmail.com>
2018-01-31 15:28:21 +0100

    WIP

--- src/som/primitives/HttpPrims.java
+++ src/som/primitives/HttpPrims.java
@@ -2,13 +2,21 @@ package som.primitives;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.util.concurrent.ForkJoinPool;
 
+import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
 
 import bd.primitives.Primitive;
+import som.VM;
+import som.interpreter.actors.EventualMessage;
+import som.interpreter.actors.SFarReference;
+import som.interpreter.nodes.nary.BinaryComplexOperation.BinarySystemOperation;
 import som.interpreter.nodes.nary.BinaryExpressionNode;
 import som.interpreter.nodes.nary.QuaternaryExpressionNode;
 import som.interpreter.nodes.nary.TernaryExpressionNode;
@@ -17,11 +25,11 @@ import som.vm.constants.Classes;
 import som.vm.constants.Nil;
 import som.vmobjects.SArray;
 import som.vmobjects.SArray.SMutableArray;
-import som.vmobjects.SBlock;
 import som.vmobjects.SClass;
 import som.vmobjects.SHttpServer;
 import som.vmobjects.SHttpServer.SHttpRequest;
 import som.vmobjects.SHttpServer.SHttpResponse;
+import som.vmobjects.SObject;
 import som.vmobjects.SSymbol;
 
 
@@ -63,11 +71,20 @@ public class HttpPrims {
   @GenerateNodeFactory
   @ImportStatic(HttpPrims.class)
   @Primitive(primitive = "httpCreateServer:port:")
-  public abstract static class HttpCreateServerPrim extends BinaryExpressionNode {
+  public abstract static class HttpCreateServerPrim extends BinarySystemOperation {
+    @CompilationFinal ForkJoinPool actorPool;
+
+    @Override
+    public BinarySystemOperation initialize(final VM vm) {
+      actorPool = vm.getActorPool();
+      return super.initialize(vm);
+    }
+
     @Specialization
     public final Object createFileDescriptor(final String adress, final long port) {
       try {
-        return new SHttpServer(new InetSocketAddress(adress, (int) port));
+        return new SHttpServer(new InetSocketAddress(adress, (int) port), vm.getActorPool(),
+            vm.getLanguage());
       } catch (IOException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
@@ -102,11 +119,22 @@ public class HttpPrims {
   @ImportStatic(HttpPrims.class)
   @Primitive(primitive = "httpRegisterHandle:method:path:handler:")
   public abstract static class HttpRegisterHandler extends QuaternaryExpressionNode {
+
     @Specialization
     public final SHttpServer register(final SHttpServer server, final SSymbol method,
         final String path,
-        final SBlock handler) {
+        final SFarReference handler) {
       server.registerHandler(path, method, handler);
+      this.getRootNode();
+      return server;
+    }
+
+    @Specialization
+    public final SHttpServer register(final SHttpServer server, final SSymbol method,
+        final String path,
+        final SObject handler) {
+      server.registerHandler(path, method,
+          new SFarReference(EventualMessage.getActorCurrentMessageIsExecutionOn(), handler));
       return server;
     }
   }
@@ -156,11 +184,48 @@ public class HttpPrims {
 
   @GenerateNodeFactory
   @ImportStatic(HttpPrims.class)
+  @Primitive(primitive = "httpRequestGetBody:")
+  public abstract static class HttpGetBodyPrim extends UnaryExpressionNode {
+    @Specialization
+    public final Object getBody(final SHttpRequest request) {
+      return request.getBody();
+    }
+  }
+
+  @GenerateNodeFactory
+  @ImportStatic(HttpPrims.class)
+  @Primitive(primitive = "httpRequestGetQuery:")
+  public abstract static class HttpGetDecodedUrlPrim extends UnaryExpressionNode {
+    @Specialization
+    public final Object getUrl(final SHttpRequest request) {
+      if (request.getExchange().getRequestURI().getQuery() != null) {
+        return request.getExchange().getRequestURI().getQuery();
+      }
+
+      try {
+        return URLDecoder.decode(request.getBody(), "utf-8");
+      } catch (UnsupportedEncodingException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+      return "";
+    }
+  }
+
+  @GenerateNodeFactory
+  @ImportStatic(HttpPrims.class)
   @Primitive(primitive = "httpRequest:getAttribute:")
   public abstract static class HttpGetAttributerPrim extends BinaryExpressionNode {
     @Specialization
     public final Object setClass(final SHttpRequest request, final String key) {
-      return request.getExchange().getAttribute(key);
+      Object result = request.getAttribute(key);
+      if (result == null) {
+        return Nil.nilObject;
+      }
+      if (result instanceof Integer) {
+        return ((Integer) result).longValue();
+      }
+      return result;
     }
   }
 
@@ -171,8 +236,18 @@ public class HttpPrims {
     @Specialization
     public final SHttpRequest setClass(final SHttpRequest request, final String key,
         final Object value) {
-      request.getExchange().setAttribute(key, value);
+      request.setAttribute(key, value);
       return request;
+    }
+  }
+
+  @GenerateNodeFactory
+  @ImportStatic(HttpPrims.class)
+  @Primitive(primitive = "httpRequest:getCookie:")
+  public abstract static class HttpGetCookiePrim extends BinaryExpressionNode {
+    @Specialization
+    public final Object setClass(final SHttpRequest request, final String key) {
+      return request.getCookie(key);
     }
   }
 
@@ -201,10 +276,15 @@ public class HttpPrims {
         e.printStackTrace();
       }
 
+      response.setClosed(true);
       response.getExchange().close();
-      response.getExchange().setAttribute("_done", true);
-      // TODO
+
       return response;
+    }
+
+    @Specialization
+    public final SHttpResponse setClass(final SHttpResponse response, final Object body) {
+      return setClass(response, body.toString());
     }
   }
 
--- src/som/vmobjects/SHttpServer.java
+++ src/som/vmobjects/SHttpServer.java
@@ -1,22 +1,37 @@
 package som.vmobjects;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
 
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
+import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.source.SourceSection;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import som.interpreter.nodes.dispatch.BlockDispatchNode;
-import som.interpreter.nodes.dispatch.BlockDispatchNodeGen;
+import som.VM;
+import som.compiler.AccessModifier;
+import som.interpreter.SomLanguage;
+import som.interpreter.actors.Actor;
+import som.interpreter.actors.EventualMessage;
+import som.interpreter.actors.EventualMessage.DirectMessage;
+import som.interpreter.actors.ReceivedMessage;
+import som.interpreter.actors.SFarReference;
+import som.interpreter.nodes.MessageSendNode;
+import som.interpreter.nodes.MessageSendNode.AbstractMessageSendNode;
 import som.vm.Symbols;
 
 
@@ -24,11 +39,12 @@ public class SHttpServer extends SObjectWithClass {
   @CompilationFinal public static SClass httpServerClass;
   @CompilationFinal public static SClass httpRequestClass;
   @CompilationFinal public static SClass httpResponseClass;
-  protected BlockDispatchNode            dispatchHandler = BlockDispatchNodeGen.create();
 
-  private final HashMap<String, HttpHandler> contexts;
-
-  private final HttpServer server;
+  private final HttpServer   server;
+  private final Actor        serverActor;
+  private final ForkJoinPool actorPool;
+  private final SomLanguage  language;
+  private final PathNode     root;
 
   public static void setSOMClass(final SClass cls) {
     httpServerClass = cls;
@@ -42,11 +58,22 @@ public class SHttpServer extends SObjectWithClass {
     httpResponseClass = cls;
   }
 
-  public SHttpServer(final InetSocketAddress address) throws IOException {
+  public SHttpServer(final InetSocketAddress address, final ForkJoinPool actorPool,
+      final SomLanguage language)
+      throws IOException {
     super(httpServerClass, httpServerClass.getInstanceFactory());
-    server = HttpServer.create(address, 0);
-    server.setExecutor(null);
-    contexts = new HashMap<>();
+    this.server = HttpServer.create(address, 0);
+    this.server.setExecutor(null);
+    this.actorPool = actorPool;
+    this.language = language;
+    this.serverActor = EventualMessage.getActorCurrentMessageIsExecutionOn();
+    this.root = new PathNode();
+    server.createContext("/", new HttpHandler() {
+      @Override
+      public void handle(final HttpExchange exchange) throws IOException {
+        root.handle(exchange.getRequestURI().getPath(), exchange);
+      }
+    });
   }
 
   @Override
@@ -58,66 +85,97 @@ public class SHttpServer extends SObjectWithClass {
     return server;
   }
 
-  public void registerHandler(final String path, final SSymbol method, final SBlock handler) {
-    if (!contexts.containsKey(path)) {
-      DynamicHttpHandler dh = new DynamicHttpHandler(path);
-      server.createContext(path, dh);
-      contexts.put(path, dh);
+  public void registerHandler(final String path, final SSymbol method,
+      final SFarReference handler) {
+    String p = path;
+    if (p.contains(":")) {
+      p = p.substring(0, p.indexOf(":"));
     }
-    if (contexts.get(path) instanceof DynamicHttpHandler) {
-      ((DynamicHttpHandler) contexts.get(path)).addHandler(method, handler);
-    } else {
-      // TODO error, path already taken by a static HttpHandler
-    }
+    VM.println(p);
+
+    DynamicHttpHandler dyn = new DynamicHttpHandler(path);
+    dyn.addHandler(method, handler);
+
+    root.registerHandler(p, dyn);
   }
 
   public void registerHandler(final String path, final String root) {
-    if (!contexts.containsKey(path)) {
-      StaticHttpHandler sh = new StaticHttpHandler(root, path);
-      server.createContext(path, sh);
-      contexts.put(path, sh);
-    } else {
-      // TODO error, path already taken by a different handler
-    }
+    this.root.registerHandler(path, new StaticHttpHandler(root, path));
   }
 
   class DynamicHttpHandler implements com.sun.net.httpserver.HttpHandler {
-    final String                              path;
-    final HashMap<SSymbol, ArrayList<SBlock>> handlers;
+    final String                                     path;
+    final HashMap<SSymbol, ArrayList<SFarReference>> handlers;
 
     public DynamicHttpHandler(final String path) {
       this.path = path;
       handlers = new HashMap<>(2);
     }
 
-    public void addHandler(final SSymbol method, final SBlock handler) {
+    public void addHandler(final SSymbol method, final SFarReference handler) {
       if (!handlers.containsKey(method)) {
         handlers.put(method, new ArrayList<>());
       }
       handlers.get(method).add(handler);
     }
 
+    private RootCallTarget createOnReceiveCallTarget(final SSymbol selector,
+        final SourceSection source, final SomLanguage lang) {
+
+      AbstractMessageSendNode invoke = MessageSendNode.createGeneric(selector, null, source);
+      ReceivedMessage receivedMsg = new ReceivedMessage(invoke, selector, lang);
+
+      return Truffle.getRuntime().createCallTarget(receivedMsg);
+    }
+
     @Override
     public void handle(final HttpExchange exch) throws IOException {
-      // TODO parse parameters from request path
+      SHttpResponse response = new SHttpResponse(exch);
+      SHttpRequest request = new SHttpRequest(exch);
+
+      if (path.contains(":")) {
+        String[] requesPath = exch.getRequestURI().getPath().toString().split("/");
+        String[] handlerPath = path.split("/");
+        if (requesPath.length >= handlerPath.length) {
+          for (int i = 0; i < handlerPath.length; i++) {
+            if (handlerPath[i].startsWith(":")) {
+              request.attributes.put("params." + handlerPath[i].substring(1), requesPath[i]);
+            }
+          }
+        } else {
+          // TODO error
+        }
+      }
 
       // call handler that are registered for this method
       SSymbol requestMethod = Symbols.symbolFor(exch.getRequestMethod());
       if (handlers.containsKey(requestMethod)) {
 
-        SHttpResponse response = new SHttpResponse(exch);
-        SHttpRequest request = new SHttpRequest(exch);
-
-        for (SBlock block : handlers.get(requestMethod)) {
-
+        for (SFarReference obj : handlers.get(requestMethod)) {
           // check that response hasnt been sent by a previous handler
-          if (exch.getAttribute("_done") != null) {
+          if (response.isClosed()) {
             break;
           }
 
-          // invoke block
-          dispatchHandler.executeDispatch(new Object[] {block, request, response});
+          SSymbol selector = Symbols.symbolFor("value:with:");
+
+          SAbstractObject o = (SAbstractObject) obj.getValue();
+
+          SInvokable s =
+              (SInvokable) o.getSOMClass().lookupMessage(selector, AccessModifier.PUBLIC);
+
+          RootCallTarget rct = createOnReceiveCallTarget(selector,
+              s.getSourceSection(), language);
+
+          DirectMessage msg =
+              new DirectMessage(obj.getActor(), selector,
+                  new Object[] {obj.getValue(), request, response}, serverActor, null, rct,
+                  false, false);
+
+          obj.getActor().send(msg, actorPool);
         }
+      } else {
+        VM.println("ignored Request");
       }
     }
   }
@@ -127,12 +185,17 @@ public class SHttpServer extends SObjectWithClass {
     private final String base;
 
     public StaticHttpHandler(final String root, final String base) {
-      this.root = root;
+      if (root.endsWith("/")) {
+        this.root = root;
+      } else {
+        this.root = root + "/";
+      }
       this.base = base;
     }
 
     @Override
     public void handle(final HttpExchange t) throws IOException {
+
       URI uri = t.getRequestURI();
       assert uri.getPath().startsWith(base);
 
@@ -144,6 +207,7 @@ public class SHttpServer extends SObjectWithClass {
         String response = "404 (Not Found)\n";
         t.sendResponseHeaders(404, response.length());
         OutputStream os = t.getResponseBody();
+        VM.println("" + uri);
         os.write(response.getBytes());
         os.close();
       } else {
@@ -176,11 +240,119 @@ public class SHttpServer extends SObjectWithClass {
     }
   }
 
+  class PathNode {
+    HashMap<String, PathNode> children;
+    HttpHandler               handler;
+
+    public void addChild(final String context, final PathNode child) {
+      if (children == null) {
+        children = new HashMap<>();
+      }
+      children.put(context, child);
+    }
+
+    public void registerHandler(final String path, final HttpHandler h) {
+      String[] components;
+      if (path.startsWith("/")) {
+        components = path.substring(1).split("/", 2);
+      } else {
+        components = path.split("/", 2);
+      }
+
+      if (components.length > 0) {
+        if (components[0].equals("")) {
+          if (this.handler == null) {
+            this.handler = h;
+          } else {
+            if (this.handler instanceof DynamicHttpHandler
+                && h instanceof DynamicHttpHandler) {
+              ((DynamicHttpHandler) handler).handlers.putAll(
+                  ((DynamicHttpHandler) h).handlers);
+            } else {
+              // TODO exception
+            }
+          }
+        } else {
+          if (!hasChild(components[0])) {
+            this.addChild(components[0], new PathNode());
+          }
+
+          if (components.length == 2) {
+            getChild(components[0]).registerHandler(components[1], h);
+          } else {
+
+            if (getChild(components[0]).handler == null) {
+              getChild(components[0]).handler = h;
+            } else {
+              getChild(components[0]).registerHandler("", h);
+            }
+          }
+        }
+      }
+    }
+
+    public PathNode getChild(final String context) {
+      if (children == null || !children.containsKey(context)) {
+        return null;
+      } else {
+        return children.get(context);
+      }
+    }
+
+    public boolean hasChild(final String context) {
+      if (children == null || !children.containsKey(context)) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    public void handle(final String context, final HttpExchange exchange) {
+      String[] components;
+      if (context.startsWith("/")) {
+        components = context.substring(1).split("/", 2);
+      } else {
+        components = context.split("/", 2);
+      }
+
+      if (components.length == 2 && hasChild(components[0])) {
+        getChild(components[0]).handle(components[1], exchange);
+      } else if (components.length == 1 && hasChild(components[0])) {
+        getChild(components[0]).accept(exchange);
+      } else {
+        this.accept(exchange);
+      }
+    }
+
+    public void accept(final HttpExchange exchange) {
+      if (handler == null) {
+        try {
+          String response = "404 (Not Found)\n";
+          exchange.sendResponseHeaders(404, response.length());
+          OutputStream os = exchange.getResponseBody();
+          os.write(response.getBytes());
+          os.close();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      } else {
+        try {
+          handler.handle(exchange);
+        } catch (IOException e) {
+          // TODO
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
   public class SHttpResponse extends SObjectWithClass {
 
     private final HttpExchange exchange;
     // default is OK
-    private long status = 200;
+    private long    status = 200;
+    private boolean closed = false;
 
     public SHttpResponse(final HttpExchange exchange) {
       super(httpResponseClass, httpResponseClass.getInstanceFactory());
@@ -200,6 +372,14 @@ public class SHttpServer extends SObjectWithClass {
       return status;
     }
 
+    public boolean isClosed() {
+      return closed;
+    }
+
+    public void setClosed(final boolean closed) {
+      this.closed = closed;
+    }
+
     public void setStatus(final long status) {
       this.status = status;
     }
@@ -207,11 +387,18 @@ public class SHttpServer extends SObjectWithClass {
 
   public class SHttpRequest extends SObjectWithClass {
 
-    private final HttpExchange exchange;
+    private final HttpExchange      exchange;
+    private final String            body;
+    private HashMap<String, String> cookies;
+    private HashMap<String, Object> attributes;
 
     public SHttpRequest(final HttpExchange exchange) {
       super(httpRequestClass, httpRequestClass.getInstanceFactory());
       this.exchange = exchange;
+      this.body = new BufferedReader(
+          new InputStreamReader(exchange.getRequestBody())).lines().collect(
+              Collectors.joining("\n"));
+      this.attributes = new HashMap<>();
     }
 
     @Override
@@ -221,6 +408,35 @@ public class SHttpServer extends SObjectWithClass {
 
     public HttpExchange getExchange() {
       return exchange;
+    }
+
+    public String getBody() {
+      return this.body;
+    }
+
+    public void setAttribute(final String key, final Object value) {
+      attributes.put(key, value);
+    }
+
+    public Object getAttribute(final String key) {
+      return attributes.get(key);
+    }
+
+    public Object getCookie(final String key) {
+      if (cookies == null) {
+        cookies = new HashMap<>();
+        for (String entry : exchange.getRequestHeaders().get("Cookie")) {
+
+          for (String e : entry.split(";")) {
+            String[] keyval = e.split("=");
+            if (keyval.length == 2) {
+              cookies.put(keyval[0].trim(), keyval[1].trim());
+            }
+          }
+        }
+      }
+
+      return cookies.get(key);
     }
   }
 }
