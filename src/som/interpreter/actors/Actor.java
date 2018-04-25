25b24889646b4aae00c20ea5e4a8a5edf5357f85
Stefan Marr <git@stefan-marr.de>
2018-04-25 23:56:13 +0100

    Add second node for TraceActorContext
    This one is needed for all buffer changes
    
    Remove recordActorCreation, was replaced by node
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/actors/Actor.java
+++ src/som/interpreter/actors/Actor.java
@@ -28,6 +28,8 @@ import tools.concurrency.KomposTrace;
 import tools.concurrency.TracingActivityThread;
 import tools.concurrency.TracingActors.ReplayActor;
 import tools.concurrency.TracingActors.TracingActor;
+import tools.concurrency.nodes.TraceActorContext;
+import tools.concurrency.nodes.TraceActorContextNodeGen;
 import tools.debugger.WebDebugger;
 import tools.debugger.entities.ActivityType;
 import tools.debugger.entities.DynamicScopeType;
@@ -230,6 +232,8 @@ public class Actor implements Activity {
       this.vm = vm;
     }
 
+    private static final TraceActorContext tracer = TraceActorContextNodeGen.create();
+
     @Override
     public void run() {
       assert executorRoot != null : "Actor system not initalized, call to initializeActorSystem(.) missing?";
@@ -249,7 +253,7 @@ public class Actor implements Activity {
       t.currentlyExecutingActor = actor;
 
       if (VmSettings.ACTOR_TRACING) {
-        ActorExecutionTrace.recordActorContext((TracingActor) actor);
+        ActorExecutionTrace.recordActorContext((TracingActor) actor, tracer);
       } else if (VmSettings.KOMPOS_TRACING) {
         KomposTrace.currentActivity(actor);
       }
