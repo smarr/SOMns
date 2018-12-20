9926f6b3734d8145676f8a437fdc27e190da09c6
Dominik Aumayr <daumayr162@gmail.com>
2018-12-20 10:56:57 +0100

    SavinaSnap changes

--- tests/snapshot-replay/test.sh
+++ tests/snapshot-replay/test.sh
@@ -12,9 +12,9 @@ then
     "ThreadRing 1 0 100:10000"
     "Chameneos 1 0 100:10000"
     "BigContention 1 0 20:12"
-    #"ConcurrentDictionary 1 0 5:100:5" chicken egg problem with entry class
-    #"ConcurrentSortedLinkedList 1 0 10:1500:10:1"
-    "ProducerConsumerBoundedBuffer 1 0 40:10:10:60"
+    "ConcurrentDictionary 1 0 5:100:5"
+    "ConcurrentSortedLinkedList 1 0 10:1500:10:1"
+    #"ProducerConsumerBoundedBuffer 1 0 40:10:10:60"
     "Philosophers 1 0 20:5000"
 
   )
@@ -23,11 +23,11 @@ else
     #"SleepingBarber 1 0 2500:1000:1000:1000"
     "CigaretteSmokers 1 0 10000:200"
     "LogisticsMapSeries 1 0 25000:10:346"
-    #"BankTransaction 1 0 1000:100000" chicken egg
-    #"RadixSort 1 0 50000:65536:74755" #not reliable
-    #"UnbalancedCobwebbedTree 1 0 100000:10:500:100" #some kind of memory leak or infinite deserialization...
+    "BankTransaction 1 0 500:10000"
+    #"RadixSort 1 0 50000:65536:74755"
+    "UnbalancedCobwebbedTree 1 0 100000:10:500:100"
     "TrapezoidalApproximation 1 0 100:1000000:1:5"
-    #"AStarSearch 1 0 100:20" #same issue as unblanced
+    #"AStarSearch 1 0 100:20"
     "NQueens 1 0 20:10:4"
   )
 fi
