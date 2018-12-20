9147c7a6030f3962ba8a61f786a896c665f40df1
Stefan Marr <git@stefan-marr.de>
2018-12-20 14:59:11 +0100

    Make explicit that object layout assumption is about latest layout
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/objectstorage/ObjectLayout.java
+++ src/som/interpreter/objectstorage/ObjectLayout.java
@@ -95,7 +95,7 @@ public final class ObjectLayout {
     latestLayoutForClass.check();
   }
 
-  Assumption getAssumption() {
+  public Assumption getIsLatestAssumption() {
     return latestLayoutForClass;
   }
 
