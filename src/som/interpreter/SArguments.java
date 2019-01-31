03bdb0261f3adf030194c99c8adca2e36ffa22db
carmen <carmentorreslopez87@gmail.com>
2019-01-31 11:07:10 +0100

    Fixes to see the trace when an error is thrown
    - commented assertion in SArguments in order to avoid an assertion error
    - add code to print the stack trace when an error is thrown in a SOMns program

--- src/som/interpreter/SArguments.java
+++ src/som/interpreter/SArguments.java
@@ -191,7 +191,7 @@ public final class SArguments {
 
   public static void setShadowStackEntry(final Object[] args,
       final ShadowStackEntry entry) {
-    assert args[args.length - 1] == null : "Assume shadow stack entry is not already set.";
+    // assert args[args.length - 1] == null : "Assume shadow stack entry is not already set.";
     args[args.length - 1] = entry;
   }
 
