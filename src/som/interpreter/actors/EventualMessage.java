f71f16166ea6723da0c6f78f3cbcee68a645b4c3
Stefan Marr <git@stefan-marr.de>
2019-02-05 00:22:56 +0000

    Fix formatting
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/actors/EventualMessage.java
+++ src/som/interpreter/actors/EventualMessage.java
@@ -398,8 +398,7 @@ public abstract class EventualMessage {
      */
     @CompilationFinal protected SPromise promise;
 
-    protected AbstractPromiseCallbackMessage(final Actor owner,
-        final SBlock callback,
+    protected AbstractPromiseCallbackMessage(final Actor owner, final SBlock callback,
         final SResolver resolver, final RootCallTarget onReceive,
         final boolean triggerMessageReceiverBreakpoint,
         final boolean triggerPromiseResolverBreakpoint, final SPromise promiseRegisteredOn) {
@@ -467,8 +466,7 @@ public abstract class EventualMessage {
   }
 
   public static final class PromiseCallbackMessage extends AbstractPromiseCallbackMessage {
-    public PromiseCallbackMessage(final Actor owner,
-        final SBlock callback,
+    public PromiseCallbackMessage(final Actor owner, final SBlock callback,
         final SResolver resolver, final RootCallTarget onReceive,
         final boolean triggerMessageReceiverBreakpoint,
         final boolean triggerPromiseResolverBreakpoint, final SPromise promiseRegisteredOn) {
