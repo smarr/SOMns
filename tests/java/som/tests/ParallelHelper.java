88d52024a6bc2523482cb38af0367c8fe102a02f
Stefan Marr <git@stefan-marr.de>
2019-02-08 22:50:01 +0000

    Change ParallelHelper to provide thread id
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/java/som/tests/ParallelHelper.java
+++ tests/java/som/tests/ParallelHelper.java
@@ -9,16 +9,18 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.IntFunction;
 
 
 public final class ParallelHelper {
 
-  public static void executeNTimesInParallel(final Runnable task) throws InterruptedException {
+  public static void executeNTimesInParallel(final IntFunction<Void> task)
+      throws InterruptedException {
     executeNTimesInParallel(task, 10);
   }
 
-  public static void executeNTimesInParallel(final Runnable task, final int timeoutInSeconds)
-      throws InterruptedException {
+  public static void executeNTimesInParallel(final IntFunction<Void> task,
+      final int timeoutInSeconds) throws InterruptedException {
     int numThreads = getNumberOfThreads();
 
     ExecutorService threadPool = Executors.newFixedThreadPool(numThreads);
@@ -30,12 +32,13 @@ public final class ParallelHelper {
       CountDownLatch threadsDone = new CountDownLatch(numThreads);
 
       for (int i = 0; i < numThreads; i++) {
+        int id = i;
         threadPool.submit(() -> {
           try {
             threadsInitialized.countDown();
             threadsInitialized.await();
 
-            task.run();
+            task.apply(id);
           } catch (Throwable t) {
             exceptions.add(t);
           }
