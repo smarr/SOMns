22a70b254fc2e74f09989f6495f6e0fe74c724a0
Dominik Aumayr <daumayr162@gmail.com>
2019-11-29 11:31:04 +0100

    atomic dispatching of blocks

--- src/som/interpreter/nodes/dispatch/BlockDispatchNode.java
+++ src/som/interpreter/nodes/dispatch/BlockDispatchNode.java
@@ -6,7 +6,9 @@ import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.api.nodes.IndirectCallNode;
 import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RootNode;
 
+import som.interpreter.Invokable;
 import som.vmobjects.SBlock;
 import som.vmobjects.SInvokable;
 
@@ -34,15 +36,46 @@ public abstract class BlockDispatchNode extends Node {
         getMethod(arguments).getCallTarget());
   }
 
+  protected static final DirectCallNode createAtomicCallNode(final Object[] arguments) {
+    return Truffle.getRuntime().createDirectCallNode(
+        getMethod(arguments).getAtomicCallTarget());
+  }
+
   protected static final IndirectCallNode createIndirectCall() {
     return Truffle.getRuntime().createIndirectCallNode();
   }
 
-  @Specialization(guards = "isSameMethod(arguments, cached)")
+  @Specialization(guards = {"isSameMethod(arguments, cached)", "forAtomic()"})
+  public Object activateCachedAtomicBlock(final Object[] arguments,
+      @Cached("getMethod(arguments)") final SInvokable cached,
+      @Cached("createAtomicCallNode(arguments)") final DirectCallNode call) {
+    return call.call(arguments);
+  }
+
+  @Specialization(guards = {"isSameMethod(arguments, cached)"})
   public Object activateCachedBlock(final Object[] arguments,
       @Cached("getMethod(arguments)") final SInvokable cached,
       @Cached("createCallNode(arguments)") final DirectCallNode call) {
     return call.call(arguments);
+  }
+
+  protected boolean forAtomic() {
+    // TODO: seems a bit expensive,
+    // might want to optimize for interpreter first iteration speed
+    RootNode root = getRootNode();
+    if (root instanceof Invokable) {
+      return ((Invokable) root).isAtomic();
+    } else {
+      // TODO: need to think about integration with actors, but, that's a
+      // later research project
+      return false;
+    }
+  }
+
+  @Specialization(replaces = "activateCachedAtomicBlock", guards = "forAtomic()")
+  public Object activateBlockAtomic(final Object[] arguments,
+      @Cached("createIndirectCall()") final IndirectCallNode indirect) {
+    return indirect.call(getMethod(arguments).getAtomicCallTarget(), arguments);
   }
 
   @Specialization(replaces = "activateCachedBlock")
@@ -50,4 +83,5 @@ public abstract class BlockDispatchNode extends Node {
       @Cached("createIndirectCall()") final IndirectCallNode indirect) {
     return indirect.call(getMethod(arguments).getCallTarget(), arguments);
   }
+
 }
