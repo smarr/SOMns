474e509b70ac5445baf9379a109e924826bc2fd6
Stefan Marr <git@stefan-marr.de>
2020-04-11 17:25:52 +0100

    Tag LogPrim with OpArithmetic
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/arithmetic/LogPrim.java
+++ src/som/primitives/arithmetic/LogPrim.java
@@ -2,15 +2,25 @@ package som.primitives.arithmetic;
 
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.instrumentation.Tag;
 
 import bd.primitives.Primitive;
 import som.interpreter.nodes.nary.UnaryBasicOperation;
+import tools.dym.Tags.OpArithmetic;
 
 
 @GenerateNodeFactory
 @Primitive(primitive = "doubleLog:", selector = "log", receiverType = Double.class)
 public abstract class LogPrim extends UnaryBasicOperation {
-  // TODO: assign some specific tag
+  @Override
+  protected boolean hasTagIgnoringEagerness(final Class<? extends Tag> tag) {
+    if (tag == OpArithmetic.class) { // TODO: is this good enough?
+      return true;
+    } else {
+      return super.hasTagIgnoringEagerness(tag);
+    }
+  }
+
   @Specialization
   public final double doLog(final double rcvr) {
     return Math.log(rcvr);
