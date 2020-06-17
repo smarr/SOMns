7b5672e0c2f39302562d380cea21c069c3323687
carmen <carmentorreslopez87@gmail.com>
2020-06-17 12:28:18 +0200

    Add stack shadow entry for do primitive
    - add stack shadow entry for do: primitive in arrays

--- src/som/primitives/arrays/DoPrim.java
+++ src/som/primitives/arrays/DoPrim.java
@@ -5,6 +5,8 @@ import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 
 import bd.primitives.Primitive;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import som.interpreter.SArguments;
 import som.interpreter.nodes.ExpressionNode;
 import som.interpreter.nodes.dispatch.BlockDispatchNode;
 import som.interpreter.nodes.dispatch.BlockDispatchNodeGen;
@@ -14,28 +16,31 @@ import som.vm.constants.Nil;
 import som.vmobjects.SArray;
 import som.vmobjects.SArray.PartiallyEmptyArray;
 import som.vmobjects.SBlock;
+import tools.asyncstacktraces.ShadowStackEntryLoad;
 
 
 @GenerateNodeFactory
 @Primitive(selector = "do:", receiverType = SArray.class, disabled = true)
 public abstract class DoPrim extends BinaryComplexOperation {
   @Child private BlockDispatchNode block = BlockDispatchNodeGen.create();
+  @Child protected ShadowStackEntryLoad shadowStackEntryLoad = ShadowStackEntryLoad.create();
 
   // TODO: tag properly, it is a loop and an access
 
-  private void execBlock(final SBlock block, final Object arg) {
-    this.block.executeDispatch(new Object[] {block, arg});
+  private void execBlock(final VirtualFrame frame, final SBlock block, final Object arg) {
+    this.block.executeDispatch(SArguments.getPlainXArgumentsWithReceiver(this,
+            shadowStackEntryLoad, frame, block, arg));
   }
 
   @Specialization(guards = "arr.isEmptyType()")
-  public final SArray doEmptyArray(final SArray arr, final SBlock block) {
+  public final SArray doEmptyArray(final VirtualFrame frame, final SArray arr, final SBlock block) {
     int length = arr.getEmptyStorage();
     try {
       if (SArray.FIRST_IDX < length) {
-        execBlock(block, Nil.nilObject);
+        execBlock(frame, block, Nil.nilObject);
       }
       for (long i = SArray.FIRST_IDX + 1; i < length; i++) {
-        execBlock(block, Nil.nilObject);
+        execBlock(frame, block, Nil.nilObject);
       }
     } finally {
       if (CompilerDirectives.inInterpreter()) {
@@ -46,15 +51,15 @@ public abstract class DoPrim extends BinaryComplexOperation {
   }
 
   @Specialization(guards = "arr.isPartiallyEmptyType()")
-  public final SArray doPartiallyEmptyArray(final SArray arr, final SBlock block) {
+  public final SArray doPartiallyEmptyArray(final VirtualFrame frame, final SArray arr, final SBlock block) {
     PartiallyEmptyArray storage = arr.getPartiallyEmptyStorage();
     int length = storage.getLength();
     try {
       if (SArray.FIRST_IDX < length) {
-        execBlock(block, storage.get(SArray.FIRST_IDX));
+        execBlock(frame, block, storage.get(SArray.FIRST_IDX));
       }
       for (long i = SArray.FIRST_IDX + 1; i < length; i++) {
-        execBlock(block, storage.get(i));
+        execBlock(frame, block, storage.get(i));
       }
     } finally {
       if (CompilerDirectives.inInterpreter()) {
@@ -65,15 +70,15 @@ public abstract class DoPrim extends BinaryComplexOperation {
   }
 
   @Specialization(guards = "arr.isObjectType()")
-  public final SArray doObjectArray(final SArray arr, final SBlock block) {
+  public final SArray doObjectArray(final VirtualFrame frame, final SArray arr, final SBlock block) {
     Object[] storage = arr.getObjectStorage();
     int length = storage.length;
     try {
       if (SArray.FIRST_IDX < length) {
-        execBlock(block, storage[SArray.FIRST_IDX]);
+        execBlock(frame, block, storage[SArray.FIRST_IDX]);
       }
       for (long i = SArray.FIRST_IDX + 1; i < length; i++) {
-        execBlock(block, storage[(int) i]);
+        execBlock(frame, block, storage[(int) i]);
       }
     } finally {
       if (CompilerDirectives.inInterpreter()) {
@@ -84,15 +89,15 @@ public abstract class DoPrim extends BinaryComplexOperation {
   }
 
   @Specialization(guards = "arr.isLongType()")
-  public final SArray doLongArray(final SArray arr, final SBlock block) {
+  public final SArray doLongArray(final VirtualFrame frame, final SArray arr, final SBlock block) {
     long[] storage = arr.getLongStorage();
     int length = storage.length;
     try {
       if (SArray.FIRST_IDX < length) {
-        execBlock(block, storage[SArray.FIRST_IDX]);
+        execBlock(frame, block, storage[SArray.FIRST_IDX]);
       }
       for (long i = SArray.FIRST_IDX + 1; i < length; i++) {
-        execBlock(block, storage[(int) i]);
+        execBlock(frame, block, storage[(int) i]);
       }
     } finally {
       if (CompilerDirectives.inInterpreter()) {
@@ -103,15 +108,15 @@ public abstract class DoPrim extends BinaryComplexOperation {
   }
 
   @Specialization(guards = "arr.isDoubleType()")
-  public final SArray doDoubleArray(final SArray arr, final SBlock block) {
+  public final SArray doDoubleArray(final VirtualFrame frame, final SArray arr, final SBlock block) {
     double[] storage = arr.getDoubleStorage();
     int length = storage.length;
     try {
       if (SArray.FIRST_IDX < length) {
-        execBlock(block, storage[SArray.FIRST_IDX]);
+        execBlock(frame, block, storage[SArray.FIRST_IDX]);
       }
       for (long i = SArray.FIRST_IDX + 1; i < length; i++) {
-        execBlock(block, storage[(int) i]);
+        execBlock(frame, block, storage[(int) i]);
       }
     } finally {
       if (CompilerDirectives.inInterpreter()) {
@@ -122,15 +127,15 @@ public abstract class DoPrim extends BinaryComplexOperation {
   }
 
   @Specialization(guards = "arr.isBooleanType()")
-  public final SArray doBooleanArray(final SArray arr, final SBlock block) {
+  public final SArray doBooleanArray(final VirtualFrame frame, final SArray arr, final SBlock block) {
     boolean[] storage = arr.getBooleanStorage();
     int length = storage.length;
     try {
       if (SArray.FIRST_IDX < length) {
-        execBlock(block, storage[SArray.FIRST_IDX]);
+        execBlock(frame, block, storage[SArray.FIRST_IDX]);
       }
       for (long i = SArray.FIRST_IDX + 1; i < length; i++) {
-        execBlock(block, storage[(int) i]);
+        execBlock(frame, block, storage[(int) i]);
       }
     } finally {
       if (CompilerDirectives.inInterpreter()) {
