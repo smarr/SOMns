0cbfee52993d92c2a23a12d34a4e41fd23875bf0
Stefan Marr <git@stefan-marr.de>
2022-09-28 14:42:01 +0100

    Use iterateFrames to remove used of getCallerFrame, which is gone from the API
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/Primitive.java
+++ src/som/interpreter/Primitive.java
@@ -79,33 +79,27 @@ public final class Primitive extends Invokable {
 
   private static Method getNextMethodOnStack() {
     return Truffle.getRuntime().iterateFrames(new FrameInstanceVisitor<Method>() {
+
       @Override
       public Method visitFrame(final FrameInstance frameInstance) {
         RootCallTarget ct = (RootCallTarget) frameInstance.getCallTarget();
         Invokable m = (Invokable) ct.getRootNode();
+
+        // the caller is a primitive, that doesn't help, we need to skip it and
+        // find a proper method
         if (m instanceof Primitive) {
           return null;
         } else {
           return (Method) m;
         }
       }
-    });
+    }, 1); // skip the first frame on stack
   }
 
   public static void propagateLoopCount(final long count) {
     CompilerAsserts.neverPartOfCompilation("Primitive.pLC(.)");
 
-    // we need to skip the primitive and get to the method that called the primitive
-    FrameInstance caller = Truffle.getRuntime().getCallerFrame();
-
-    RootCallTarget ct = (RootCallTarget) caller.getCallTarget(); // caller method
-    Invokable m = (Invokable) ct.getRootNode();
-
-    if (m instanceof Primitive) {
-      // the caller is a primitive, that doesn't help, we need to skip it and
-      // find a proper method
-      m = getNextMethodOnStack();
-    }
+    Invokable m = getNextMethodOnStack();
 
     if (m != null && !(m instanceof Primitive)) {
       m.propagateLoopCountThroughoutMethodScope(count);
