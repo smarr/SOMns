62ad83ce389491f31bc3833c1f291585f3e4cb1b
Stefan Marr <git@stefan-marr.de>
2022-10-04 11:39:55 +0100

    Fix Whitespace
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/actors/SchedulePromiseHandlerNode.java
+++ src/som/interpreter/actors/SchedulePromiseHandlerNode.java
@@ -54,15 +54,15 @@ public abstract class SchedulePromiseHandlerNode extends Node {
       msg.messageId = npr.eventNo;
     }
 
-      if (VmSettings.ACTOR_ASYNC_STACK_TRACE_STRUCTURE) {
-          // TODO: I think, we need the info about the resolution context from the promise
-          // we want to know where it was resolved, where the value is coming from
-          ShadowStackEntry resolutionEntry = ShadowStackEntry.createAtPromiseResolution(
-                                                                                        SArguments.getShadowStackEntry(frame),
-                                                                                        getParent().getParent());
-          assert !VmSettings.ACTOR_ASYNC_STACK_TRACE_STRUCTURE || resolutionEntry != null;
-          SArguments.setShadowStackEntry(msg.args, resolutionEntry);
-      }
+    if (VmSettings.ACTOR_ASYNC_STACK_TRACE_STRUCTURE) {
+      // TODO: I think, we need the info about the resolution context from the promise
+      // we want to know where it was resolved, where the value is coming from
+      ShadowStackEntry resolutionEntry = ShadowStackEntry.createAtPromiseResolution(
+          SArguments.getShadowStackEntry(frame),
+          getParent().getParent());
+      assert !VmSettings.ACTOR_ASYNC_STACK_TRACE_STRUCTURE || resolutionEntry != null;
+      SArguments.setShadowStackEntry(msg.args, resolutionEntry);
+    }
     msg.originalSender.send(msg, actorPool);
   }
 
